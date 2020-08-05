1a04b62ec1c49dc722c01b35bedf52e2ea8a5d3c
YeongG <yeonggil223@gmail.com>
2020-08-05 16:03:23 +0900

    [MODIFY] 항상 네비게이션 랜더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { FC } from 'react';
 import { Switch, BrowserRouter } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
+import { GlobalStyle } from './GlobalStyle';
 
 const App: FC<{}> = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
+      <Navigation />
       <Switch></Switch>
     </BrowserRouter>
   );
