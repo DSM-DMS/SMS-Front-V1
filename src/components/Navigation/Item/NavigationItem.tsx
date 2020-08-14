c37b92501fea0bd263bb0c12b8d4ed981b1b434f
YeongG <yeonggil223@gmail.com>
2020-08-15 02:31:03 +0900

    [FIX] NavigationItem 의존성 제거

---
+++ src/components/Navigation/Item/NavigationItem.tsx
@@ -0,0 +1,24 @@
+import React, { memo, useEffect } from 'react';
+import { FC } from 'react';
+import * as S from './styles';
+
+interface Props {
+  name: string;
+  src: string;
+  isActive: boolean;
+  onClick: () => void;
+}
+
+const NavigationItem: FC<Props> = ({ src, name, isActive, onClick }) => {
+  return (
+    <S.Container onClick={onClick} isActive={isActive}>
+      <S.Header>
+        <S.Img src={src} />
+        <S.ItemName>{name}</S.ItemName>
+      </S.Header>
+      {isActive && <S.Triangle />}
+    </S.Container>
+  );
+};
+
+export default NavigationItem;
