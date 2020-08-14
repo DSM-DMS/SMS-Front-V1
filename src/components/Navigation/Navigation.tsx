d668790252086a96cd9d91e487ab494facef5f9b
YeongG <yeonggil223@gmail.com>
2020-08-15 02:38:12 +0900

    [MODIFY] 사이트 접속시 현재 경로를 Dispatch

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,9 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import NavigationMain from './Main/NavigationMain';
 import * as S from './styles';
 import NavigationSub from './Sub/NavigationSub';
+import { useDispatch } from 'react-redux';
+import { pageMove, subPageMove } from '../../modules/action/page';
+import { getNavUrl } from '../../lib/api';
 
 const Navigation: FC = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const { mainUrl, subUrl } = getNavUrl(window.location.href);
+    dispatch(pageMove(mainUrl));
+    dispatch(subPageMove(subUrl));
+  }, []);
+
   return (
     <S.Container>
       <NavigationMain />
