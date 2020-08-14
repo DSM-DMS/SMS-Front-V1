24a50d49dc559d42bf7a2604bbb2f11cdd47c024
YeongG <yeonggil223@gmail.com>
2020-08-15 02:30:37 +0900

    [CREATE] NavigationItem 컨테이너들 생성

---
+++ src/components/Navigation/Item/MainSubNavigationItemContainer.tsx
@@ -0,0 +1,41 @@
+import React, { FC, useCallback } from 'react';
+import NavigationItem from './NavigationItem';
+import { useDispatch } from 'react-redux';
+import { pageMove, subPageMove } from '../../../modules/action/page';
+import { useHistory } from 'react-router';
+
+interface Props {
+  isActive: boolean;
+  name: string;
+  src: string;
+  route: string;
+  subUrl: string;
+}
+
+const MainSubNavigationItemContainer: FC<Props> = ({
+  isActive,
+  name,
+  src,
+  route,
+  subUrl,
+}) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const onClick = useCallback(() => {
+    dispatch(pageMove(name));
+    dispatch(subPageMove(subUrl));
+    history.push(route);
+  }, [dispatch]);
+
+  return (
+    <NavigationItem
+      isActive={isActive}
+      name={name}
+      src={src}
+      onClick={onClick}
+    />
+  );
+};
+
+export default MainSubNavigationItemContainer;
