2dee99b4e8e3c9f5ac3084cce651433e1722d96e
YeongG <yeonggil223@gmail.com>
2020-08-16 04:15:59 +0900

    [CREATE] BoardTableBody 컴포넌트 생성

---
+++ src/components/Board/Table/body/BoardTableBody.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { BoardObj } from '../../Board';
+import BoardTableItem from './item/BoardTableItem';
+
+interface Props {
+  data: BoardObj[];
+}
+
+const BoardTableBody: FC<Props> = ({ data }) => {
+  return (
+    <div>
+      {data.map(({ id, title, viewCount, date }) => (
+        <BoardTableItem
+          id={id}
+          title={title}
+          viewCount={viewCount}
+          date={date}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default BoardTableBody;
