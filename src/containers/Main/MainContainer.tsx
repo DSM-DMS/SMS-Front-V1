d9a61738abbc55b4cbfb2a0bda3a762d1c3bf925
이성진 <chlskatnr1@naver.com>
2020-08-17 21:25:43 +0900

    [CREATE] Main containers 파일 생성

---
+++ src/containers/Main/MainContainer.tsx
@@ -0,0 +1,11 @@
+import React, { FC, ReactElement } from 'react';
+
+import { Main } from '../../components';
+
+interface Props {}
+
+const MainContainer: FC<Props> = (): ReactElement => {
+  return <Main />;
+};
+
+export default MainContainer;
