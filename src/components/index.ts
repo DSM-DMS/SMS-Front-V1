b56f63b513a041ed2fb59316e0ea099871ad541d
YeongG <yeonggil223@gmail.com>
2020-08-18 10:52:09 +0900

    [MODIFY] Component/index.ts 컴포넌트 추가

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Board } from './Board/Board';
+export { default as Navigation } from './Navigation/Navigation';
+export { default as DetailPageHeader } from './PageHeader/DetailPageHeader';
+export { default as ListPageHeader } from './PageHeader/ListPageHeader';
+export { default as NoticeDetail } from './Notice/NoticeDetail/NoticeDetail';
