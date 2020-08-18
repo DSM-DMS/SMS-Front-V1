172fd574b6374f356bf8d2554b9fc50b471476e1
이성진 <chlskatnr1@naver.com>
2020-08-18 14:51:44 +0900

    [MODIFY] <components/Main/Outing/Outing.tsx> Outing styledComponents 적용

--- src/components/Main/Outing/Outing.tsx
+++ src/components/Main/Outing/Outing.tsx
@@ -3,25 +3,26 @@ import React, { FC, ReactElement } from 'react';
 import * as S from '../style';
 
 import { bike, outingHistory, warning } from '../../../assets/main';
+
 interface Props {}
 
 const Outing: FC<Props> = (): ReactElement => {
   return (
     <S.Outing>
-      <h2>외출증</h2>
+      <S.OutingTitle>외출신청</S.OutingTitle>
       <div>
-        <div>
-          <img src={warning} alt="waring" title="waring" />
-          <span>유의사항</span>
-        </div>
-        <div>
+        <S.OutingItem>
+          <img src={warning} alt="warning" title="warning" />
+          <S.OutingItemDesc>유의사항</S.OutingItemDesc>
+        </S.OutingItem>
+        <S.OutingItem>
           <img src={bike} alt="bike" title="bike" />
-          <span>외출사항</span>
-        </div>
-        <div>
+          <S.OutingItemDesc>외출사항</S.OutingItemDesc>
+        </S.OutingItem>
+        <S.OutingItem>
           <img src={outingHistory} alt="outingHistory" title="outingHistory" />
-          <span>내 외출신청 내역</span>
-        </div>
+          <S.OutingItemDesc>내 외출신청 내역</S.OutingItemDesc>
+        </S.OutingItem>
       </div>
     </S.Outing>
   );
