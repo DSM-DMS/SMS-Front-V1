116bac3cc6f5e79d80187767fd071a07bff48464
이성진 <chlskatnr1@naver.com>
2020-08-18 14:52:13 +0900

    [MODIFY] <components/Main/Schedule/Schedule.tsx> Schedule styledComponents 적용

--- src/components/Main/Schedule/Schedule.tsx
+++ src/components/Main/Schedule/Schedule.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 
 import * as S from '../style';
 import Calendar from './Calendar/Calendar';
@@ -8,17 +8,40 @@ import { arrow } from '../../../assets/main';
 interface Props {}
 
 const Schedule: FC<Props> = (): ReactElement => {
+  const [today, setToday] = useState<Date>(new Date());
+
+  const onClickNextMonth = () => {
+    setToday((prev) => new Date(prev.getFullYear(), prev.getMonth() + 1, 1));
+  };
+
+  const onClickPrevMonth = () => {
+    setToday((prev) => new Date(prev.getFullYear(), prev.getMonth() - 1, 1));
+  };
+
+  const getLocalDate = (date: Date) =>
+    `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
+
   return (
     <S.Schedule>
-      <header>
-        <h3>학사일정</h3>
-        <div>
-          <span>2020년 8월 17일</span>
-          <img src={arrow} alt="arrow" title="arrow" />
-          <img src={arrow} alt="arrow" title="arrow" className="down" />
-        </div>
-      </header>
-      <Calendar />
+      <S.ScheduleHeader>
+        <S.MainContentTitleCommon>학사일정</S.MainContentTitleCommon>
+        <S.ScheduleHeaderDateSetting>
+          <span>{getLocalDate(today)}</span>
+          <S.ScheduleArrow
+            src={arrow}
+            alt="prevMonth"
+            title="prevMonth"
+            onClick={onClickPrevMonth}
+          />
+          <S.ScheduleArrow
+            src={arrow}
+            alt="nextMonth"
+            title="nextMonth"
+            onClick={onClickNextMonth}
+          />
+        </S.ScheduleHeaderDateSetting>
+      </S.ScheduleHeader>
+      <Calendar today={today} />
     </S.Schedule>
   );
 };
