438442c398b824905f36659782ef2de8c1493b75
이성진 <chlskatnr1@naver.com>
2020-08-19 11:11:20 +0900

    [MODIFY] <routers/OutingRouter> OutingRouter containter와 연결

--- src/routers/OutingRouter.tsx
+++ src/routers/OutingRouter.tsx
@@ -1,12 +1,18 @@
 import React, { FC } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import {
+  WarningContainer,
+  ApplyContainer,
+  HistoryContainer,
+} from '../containers';
+
 const OutingRouter: FC<{}> = () => {
   return (
     <Switch>
-      <Route exact path="/outing/waring" render={() => <div>waring</div>} />
-      <Route exact path="/outing/apply" render={() => <div>apply</div>} />
-      <Route exact path="/outing/history" render={() => <div>history</div>} />
+      <Route exact path="/outing/waring" render={WarningContainer} />
+      <Route exact path="/outing/apply" render={ApplyContainer} />
+      <Route exact path="/outing/history" render={HistoryContainer} />
     </Switch>
   );
 };
