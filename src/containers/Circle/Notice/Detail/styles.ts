e5cefa86ee0fb891459bf49872ff1c90b25f6570
YeongG <yeonggil223@gmail.com>
2020-08-19 11:53:30 +0900

    [MODIFY] Style 버그 Fix

--- src/containers/Circle/Notice/Detail/styles.ts
+++ src/containers/Circle/Notice/Detail/styles.ts
@@ -2,4 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   padding: 50px 70px;
+  background-color: white;
+  box-shadow: 0px 0px 12px 1px rgba(0, 0, 0, 0.25);
 `;
