21bc57d3cc7bf5f69e979c371c08247da565ffd0
이성진 <chlskatnr1@naver.com>
2020-08-19 11:10:28 +0900

    [CREATE] <components/Outing> outing style 생성

---
+++ src/components/Outing/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const OutingWrap = styled.div``;
+
+export const WarningWarp = styled.div``;
+
+export const ApplyWarp = styled.div``;
+
+export const HistoryWarp = styled.div``;
