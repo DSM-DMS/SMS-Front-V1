d54cc51f0a73c46f36337b13740114f51a6acb74
이성진 <chlskatnr1@naver.com>
2020-08-19 10:26:48 +0900

    [MODIFY] <components/Main/Schedule/Calendar/*> Calendar 변경

--- src/components/Main/Schedule/Calendar/Calendar.tsx
+++ src/components/Main/Schedule/Calendar/Calendar.tsx
@@ -4,15 +4,18 @@ import * as S from './style';
 import CalendarDate from './CalendarDate';
 import CalendarDay from './CalendarDay';
 
+import { Schedule as ISchedule } from '../../../../containers/Main/MainContainer';
+
 interface Props {
   today: Date;
+  schedules: ISchedule[];
 }
 
-const Calendar: React.FC<Props> = ({ today }) => {
+const Calendar: React.FC<Props> = ({ today, schedules }) => {
   return (
     <S.Calendar>
       <CalendarDay />
-      <CalendarDate today={today} />
+      <CalendarDate today={today} schedules={schedules} />
     </S.Calendar>
   );
 };
--- src/components/Main/Schedule/Calendar/CalendarDate.tsx
+++ src/components/Main/Schedule/Calendar/CalendarDate.tsx
@@ -2,83 +2,103 @@ import React, { ReactElement, useMemo, MouseEvent } from 'react';
 
 import * as S from './style';
 
+import { Schedule as ISchedule } from '../../../../containers/Main/MainContainer';
+
 interface Props {
   today: Date;
+  schedules: ISchedule[];
 }
 
-const CalendarDate: React.FC<Props> = ({ today }) => {
-  const fixNum = (num: number): string => (num < 10 ? `0${num}` : String(num));
-
-  const onClickDate = (e: MouseEvent<HTMLDivElement>) => {
-    e.currentTarget.classList.add('selected');
-  };
-
-  const getDateHTML = (
-    styling: string = '',
-    children: number = 0,
-    id: string = '',
-  ): ReactElement => (
-    <S.CalendarDate key={id} className={styling} onClick={onClickDate}>
-      <S.CalendarDaySpan>{children ? fixNum(children) : ''}</S.CalendarDaySpan>
-    </S.CalendarDate>
-  );
-
+const CalendarDate: React.FC<Props> = ({ today, schedules }) => {
   const setCalendarData = (
     yearCopy: number,
     monthCopy: number,
   ): ReactElement[] => {
-    const fixedMonth: string = fixNum(monthCopy);
-    const lastDay: number = new Date(yearCopy, +fixedMonth, 0).getDate();
-    const firstDayName: number = new Date(
-      yearCopy,
-      +fixedMonth - 1,
-      1,
-    ).getDay();
-    const calJSX: ReactElement[] = [];
-    const prevLastDay = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      0,
-    ).getDate();
+    const fixedMonth: string = fixNum(monthCopy),
+      lastDay: number = new Date(yearCopy, monthCopy, 0).getDate(),
+      firstDayName: number = new Date(yearCopy, monthCopy - 1, 1).getDay(),
+      calJSX: ReactElement[] = [];
     let startDayCount: number = 1;
-    let nextMonDayCount: number = 1;
+    let nextDayCount: number = 1;
 
     for (let i = 0; i < 6; i += 1) {
       for (let j = 0; j < 7; j += 1) {
         if (i === 0 && j < firstDayName) {
-          calJSX.push(
-            getDateHTML('prev', prevLastDay - (firstDayName - 1) + j, `${j}`),
-          );
+          calJSX.push(getDateJSX(lastDay - (firstDayName - 1) + j, `${j}`));
         } else if (i >= 0 && startDayCount <= lastDay) {
-          calJSX.push(
-            getDateHTML(
-              'curr',
-              startDayCount,
-              `${yearCopy}-${fixedMonth}-${fixNum(startDayCount)}`,
-            ),
-          );
+          const date = `${yearCopy}-${fixedMonth}-${fixNum(startDayCount)}`,
+            t = new Date(),
+            y = t.getFullYear(),
+            m = t.getMonth() + 1,
+            d = t.getDate();
+          date === `${y}-${fixNum(m)}-${fixNum(d)}`
+            ? calJSX.push(getDateJSX(startDayCount, date, 'curr today'))
+            : calJSX.push(getDateJSX(startDayCount, date, 'curr'));
           startDayCount += 1;
         } else {
-          calJSX.push(
-            getDateHTML(
-              'next',
-              nextMonDayCount,
-              `${yearCopy}-${fixedMonth + 1}-${fixNum(nextMonDayCount)}`,
-            ),
-          );
-          nextMonDayCount += 1;
+          const date = `${yearCopy}-${fixedMonth + 1}-${fixNum(nextDayCount)}`;
+          calJSX.push(getDateJSX(nextDayCount, date));
+          nextDayCount += 1;
         }
       }
     }
-
     return calJSX;
+  };
+
+  const getDateJSX = (
+    children: number = 0,
+    id: string = '',
+    styling: string = '',
+  ): ReactElement => (
+    <S.CalendarDate
+      key={id}
+      className={styling}
+      onClick={styling.match('curr') ? onClickDate : () => {}}
+    >
+      <S.CalendarDaySpan>{children ? fixNum(children) : ''}</S.CalendarDaySpan>
+    </S.CalendarDate>
+  );
+
+  const fixNum = (num: number): string => (num < 10 ? `0${num}` : String(num));
+
+  const onClickDate = (e: MouseEvent<HTMLDivElement>) => {
+    e.currentTarget.parentElement.childNodes.forEach((el) => {
+      (el as HTMLDivElement).classList.remove('selected');
+    });
+    e.currentTarget.classList.add('selected');
   };
 
   const memoizedCalendar = useMemo<ReactElement[]>(
     () => setCalendarData(today.getFullYear(), today.getMonth() + 1),
     [today],
   );
-  return <>{memoizedCalendar}</>;
+
+  const setScheduleData = useMemo(() => {
+    const { endDate, schedule, startDate } = schedules[0];
+    const date = new Date();
+    const start = new Date(`${date.getFullYear()}.${startDate}`);
+    const end = new Date(`${date.getFullYear()}.${endDate}`);
+    const day: number = new Date(`${date.getFullYear()}.${startDate}`).getDay();
+    const SEC_OF_DAY = 86400 * 1000;
+    const dateCount: number =
+      (end.getTime() - start.getTime()) / SEC_OF_DAY || 1;
+    let level = 1;
+
+    // console.log(startDate, endDate, day, dateCount, level);
+
+    return (
+      <S.CalendarBar dateCount={dateCount} day={day}>
+        {schedule}
+      </S.CalendarBar>
+    );
+  }, []);
+
+  return (
+    <>
+      {memoizedCalendar}
+      {/* setScheduleData */}
+    </>
+  );
 };
 
 export default CalendarDate;
