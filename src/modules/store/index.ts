840464fb17cf9b8775971fca57182cb680f59b99
YeongG <yeonggil223@gmail.com>
2020-08-19 11:13:13 +0900

    [MODIFY] Saga 생성

--- src/modules/store/index.ts
+++ src/modules/store/index.ts
@@ -1,7 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducer';
+import rootSaga from '../saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import saga from 'redux-saga';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
+const sagaMiddleware = saga();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
