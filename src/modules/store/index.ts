c19597b27713ca2fbced76b63e13d6243c4a1a4f
YeongG <yeonggil223@gmail.com>
2020-08-19 11:14:18 +0900

    [MODIFY] Saga 삭제

--- src/modules/store/index.ts
+++ src/modules/store/index.ts
@@ -1,16 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducer';
-import rootSaga from '../saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import saga from 'redux-saga';
-
-const sagaMiddleware = saga();
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware)),
-);
-
-sagaMiddleware.run(rootSaga);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
 
 export default store;
