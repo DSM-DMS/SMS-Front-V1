34e3ad00d17142b234fa0a19bb80e6bd8c10497f
YeongG <yeonggil223@gmail.com>
2020-08-21 09:44:32 +0900

    [MODIFY] import export 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as Main } from './Main/Main';
 export { default as CircleNoticeDetail } from './Circle/Notice/Detail/CircleNoticeDetail';
 export { default as CircleNoticeList } from './Circle/Notice/List/CircleNoticeList';
 export { default as CircleWanted } from './Circle/Wanted/CircleWanted';
+export { default as CircleAll } from './Circle/All/CircleAll';
