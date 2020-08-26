1159a1bcce45407d94061710b5d08902dfa4903f
YeongG <yeonggil223@gmail.com>
2020-08-26 16:29:06 +0900

    [MODIFY] 스타일 수정

--- src/components/default/CircleBox/styles.ts
+++ src/components/default/CircleBox/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 265px;
-  height: 265px;
+  width: 300px;
+  height: 300px;
   padding: 20px;
   box-sizing: border-box;
   margin: 8px;
