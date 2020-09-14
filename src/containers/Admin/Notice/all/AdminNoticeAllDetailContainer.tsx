1c444e52e3051f0093962182d4e8ea29b23e290d
YeongG <yeonggil223@gmail.com>
2020-09-14 21:54:45 +0900

    [CREATE] Admin/Notice/All/Detail Component/Container

---
+++ src/containers/Admin/Notice/all/AdminNoticeAllDetailContainer.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { AdminNoticeAllDetail } from '../../../../components';
+import { updateBoardDetail } from '../../../../modules/action/board';
+
+const AdminNoticeAllDetailContainer: FC = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(updateBoardDetail({ content: '몰라 씨@@@@발' }));
+  }, []);
+  return <AdminNoticeAllDetail />;
+};
+
+export default AdminNoticeAllDetailContainer;
