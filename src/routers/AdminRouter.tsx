d81611505e4768f7c8b79b1bedc8793ce41267b4
YeongG <yeonggil223@gmail.com>
2020-09-14 13:21:18 +0900

    [MODIFY] Admin 라우팅 수정

--- src/routers/AdminRouter.tsx
+++ src/routers/AdminRouter.tsx
@@ -1,22 +1,39 @@
 import React, { FC } from 'react';
 import { Route, Switch } from 'react-router-dom';
-
 import {
+  AdminOutingCertifiedListContainer,
+  AdminOutingNowListContainer,
   AdminOutingWaitListContainer,
   AdminMainContainer,
 } from '../containers';
+import { GlobalInnerBody } from '../GlobalStyle';
 
 const AdminRouter: FC<{}> = () => {
   return (
-    <Switch>
-      <Route exact path="/admin/home" component={AdminMainContainer} />
-      <Route
-        exact
-        path="/admin/outing"
-        component={AdminOutingWaitListContainer}
-      />
-      <Route exact path="/admin/notice" />
-    </Switch>
+    <GlobalInnerBody>
+      <Switch>
+        <Route exact path="/admin/home" component={AdminMainContainer} />
+        <Route exact path="/admin/schedule" />
+        <Route
+          exact
+          path="/admin/out/wait"
+          component={AdminOutingWaitListContainer}
+        />
+
+        <Route
+          exact
+          path="/admin/out/now"
+          component={AdminOutingNowListContainer}
+        />
+
+        <Route
+          exact
+          path="/admin/out/certified"
+          component={AdminOutingCertifiedListContainer}
+        />
+        <Route exact path="/admin/notice" />
+      </Switch>
+    </GlobalInnerBody>
   );
 };
 
