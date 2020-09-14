787fbab187696f4bb44a692a94317622aba25dce
YeongG <yeonggil223@gmail.com>
2020-09-14 13:20:15 +0900

    [CREATE] style 변경

--- src/components/default/Board/Table/body/item/styles.ts
+++ src/components/default/Board/Table/body/item/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Container } from '../../header/styles';
+import { Container, OutingItemContainer } from '../../header/styles';
 
 export const ItemContainer = styled(Container)`
   background-color: white;
@@ -9,6 +9,18 @@ export const ItemContainer = styled(Container)`
   border-top-color: #d3d3d3;
 
   div:nth-child(2) {
+    text-align: left;
+  }
+`;
+
+export const OutingContainer = styled(OutingItemContainer)`
+  background-color: white;
+  font-weight: normal;
+  font-size: 12px;
+  border-top-width: 1px;
+  border-top-color: #d3d3d3;
+
+  div:nth-child(5) {
     text-align: left;
   }
 `;
