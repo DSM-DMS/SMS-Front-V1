71989f1b03b945800642e3af796af13051b69f88
YeongG <yeonggil223@gmail.com>
2020-09-23 19:18:19 +0900

    [MODIFY] store와saga 연결

--- src/modules/store/index.ts
+++ src/modules/store/index.ts
@@ -1,6 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
+import createReduxSaga from 'redux-saga';
+import rootSaga from '../saga';
+
+const reduxSaga = createReduxSaga();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(reduxSaga)),
+);
+
+reduxSaga.run(rootSaga);
 
 export default store;
