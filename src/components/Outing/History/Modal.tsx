e2fb1fb04d45c28ead1b465bfddd95680a65c42b
이성진 <chlskatnr1@naver.com>
2020-09-25 16:37:03 +0900

    [MODIFY] <Outing/History/Modal> outingState 생성

--- src/components/Outing/History/Modal.tsx
+++ src/components/Outing/History/Modal.tsx
@@ -1,16 +1,34 @@
 import React, { FC, ReactElement, useState, useEffect } from 'react';
 
-import ModalApply from './ModalApply';
-import ModalOnlineCard from './ModalOnlineCard';
+import { WithModalApply, WithModalOnlineCard } from './WithModalAni';
 
 import * as S from '../style';
+
+export const REJECT = 'reject';
+export const APPROVE = 'approve';
+export const OUT = 'out';
+export const EXPIRED = 'expired';
+
+type OutingActions =
+  | typeof REJECT
+  | typeof APPROVE
+  | typeof OUT
+  | typeof EXPIRED;
 
 interface Props {
   handleModal: (isShow: boolean) => void;
 }
 
+export interface WithModalProps {
+  handleMode: (mode: 'apply' | 'card') => void;
+  handleModal: (isShow: boolean) => void;
+  outingState: OutingActions;
+  setOutingState: (action: OutingActions) => void;
+}
+
 const Modal: FC<Props> = ({ handleModal }): ReactElement => {
   const [mode, setMode] = useState<'apply' | 'card'>('apply');
+  const [outingState, setOutingState] = useState<OutingActions>(APPROVE);
 
   const handleMode = (mode: 'apply' | 'card') => {
     setMode(mode);
@@ -27,10 +45,20 @@ const Modal: FC<Props> = ({ handleModal }): ReactElement => {
       <S.ModalBack />
       <S.ModalContentWrap>
         {mode === 'apply' && (
-          <ModalApply handleMode={handleMode} handleModal={handleModal} />
+          <WithModalApply
+            handleMode={handleMode}
+            handleModal={handleModal}
+            outingState={outingState}
+            setOutingState={setOutingState}
+          />
         )}
         {mode === 'card' && (
-          <ModalOnlineCard handleMode={handleMode} handleModal={handleModal} />
+          <WithModalOnlineCard
+            handleMode={handleMode}
+            handleModal={handleModal}
+            outingState={outingState}
+            setOutingState={setOutingState}
+          />
         )}
       </S.ModalContentWrap>
     </S.HistoryModalWrap>
