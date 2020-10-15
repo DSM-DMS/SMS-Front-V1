ba2ab79893ccf61ae5e51e400c822085fecaba81
YeongG <yeonggil223@gmail.com>
2020-10-15 15:42:51 +0900

    [MODIFY] management routing 추가

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,14 @@ import {
   OutingRouter,
   MainRouter,
   AdminRouter,
+  ManagementRouter,
 } from './routers';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { jsonActionCreater } from './modules/action/json';
 
 const App: FC<{}> = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch();  
   useEffect(() => {
     dispatch(jsonActionCreater.getJsonSaga());
   }, []);
@@ -28,11 +29,13 @@ const App: FC<{}> = () => {
         <GlobalBody>
           <Header />
           <Switch>
+          
             <Route path="/home" component={MainRouter} />
             <Route path="/notice" component={NoticeRouter} />
             <Route path="/circles" component={CirclesRouter} />
             <Route path="/outing" component={OutingRouter} />
             <Route path="/admin" component={AdminRouter} />
+            <Route path="/management" component={ManagementRouter} />
           </Switch>
         </GlobalBody>
       </BrowserRouter>
