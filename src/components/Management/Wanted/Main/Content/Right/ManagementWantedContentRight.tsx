7001d6c2baf5327558697b9536351e75f24ec78c
YeongG <yeonggil223@gmail.com>
2020-10-19 09:30:28 +0900

    [CREATE] ManagementWantedContentRight

---
+++ src/components/Management/Wanted/Main/Content/Right/ManagementWantedContentRight.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "../../../../../../styles/CircleWantedDetail";
+
+const ManagementWantedContentRight = () => {
+  return <S.ContentRightWrap></S.ContentRightWrap>;
+};
+
+export default ManagementWantedContentRight;
