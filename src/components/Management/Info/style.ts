ca1019bac8f526ba366b9864bd02b61011eab8ad
이성진 <chlskatnr1@naver.com>
2020-10-21 21:59:24 +0900

    [MODIFY] <style.ts> 동아리 정보 수정 스타일 개선

--- src/components/Management/Info/style.ts
+++ src/components/Management/Info/style.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+interface Track {
+  height: number;
+}
+
 const BASE_BACK_COLOR = "#f1f1f1";
 
 export const ManagementInfoWrap = styled.div`
@@ -154,7 +158,30 @@ export const ClubPictureInnerText = styled.span`
   font-size: 14px;
 `;
 
-export const ClubPictureLabel = styled.label`
+export const ClubPictureThumbnail = styled.label`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+  height: 200px;
+  border: 2px dashed #dddddd;
+  box-sizing: border-box;
+  color: #888888;
+  background-color: ${BASE_BACK_COLOR};
+  font-size: 12px;
+  &.dragged {
+    background-color: white;
+  }
+  > img {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    cursor: pointer;
+  }
+`;
+
+export const ClubPictureWrap = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -162,25 +189,25 @@ export const ClubPictureLabel = styled.label`
   padding: 4px;
   border: 0;
   color: #888888;
-  background-color: ${BASE_BACK_COLOR};
+  background-color: transparent;
   box-sizing: border-box;
   font-size: 15px;
   cursor: pointer;
+  z-index: 10;
   &:hover {
     color: #242424;
   }
 `;
 
-export const ClubPictureThumbnail = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 201px;
-  border: 2px dashed #dddddd;
-  box-sizing: border-box;
-  color: #888888;
-  font-size: 12px;
+export const ClubPicturePreview = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 100%;
+  max-height: 100%;
+  opacity: 0.5;
+  z-index: 1;
 `;
 
 export const ClubFacebook = styled(ClubCommonStyle)``;
@@ -233,22 +260,145 @@ export const BottomEditButton = styled(BottomButton)`
   border: 1px solid #5323b2;
 `;
 
-export const Modal = styled.div`
-  > .back {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background-color: rgba(0, 0, 0, 0.23);
-    z-index: 10;
+export const Modal = styled.div``;
+
+export const ModalBack = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.23);
+  z-index: 10;
+`;
+
+export const ModalForm = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px 40px;
+  background-color: white;
+  z-index: 11;
+  > p {
+    font-weight: bold;
   }
-  > .form {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: white;
-    z-index: 11;
+`;
+
+export const ModalFormInner = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  > div:first-child {
+    margin-right: 20px;
   }
+  > div:last-child {
+    margin-right: 20px;
+  }
+`;
+
+export const SearchingWrap = styled.div`
+  position: relative;
+`;
+
+export const SearchLabel = styled.label`
+  display: flex;
+  align-items: center;
+  margin: 12px 0;
+  padding: 4px 8px;
+  background-color: #f6f6f6;
+  box-sizing: border-box;
+`;
+
+export const SearchText = styled.input`
+  margin-left: 4px;
+  padding: 0;
+  border: 0;
+  background-color: transparent;
+  &::placeholder {
+    color: #1d1d1d;
+  }
+`;
+
+export const Result = styled.ul`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  max-height: 120px;
+  margin: 8px 0;
+  border-radius: 4px;
+  color: rgba(0, 0, 0, 0);
+  background-color: white;
+  transition: color 0.3s ease;
+  overflow-y: scroll;
+  text-shadow: 0 0 black;
+  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2);
+  &::-webkit-scrollbar,
+  &::-webkit-scrollbar-thumb {
+    width: 16px;
+    border-radius: 13px;
+    background-clip: padding-box;
+    border: 4px solid transparent;
+    box-shadow: inset 0 0 0 10px;
+  }
+  &:hover {
+    color: rgba(0, 0, 0, 0.3);
+  }
+`;
+
+export const ResultItem = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px;
+  color: #1d1d1d;
+  font-weight: 300;
+  cursor: pointer;
+  &:hover {
+    background: rgba(0, 0, 0, 0.05);
+  }
+`;
+
+export const Queue = styled.ul`
+  width: 100px;
+  margin: 8px 0;
+  > p {
+    font-size: 14px;
+    color: #838383;
+  }
+  > li {
+    transition: 0.3s ease;
+    cursor: pointer;
+    &:hover {
+      color: #ff6961;
+    }
+  }
+`;
+
+export const ModalButtonWrap = styled.div`
+  text-align: right;
+`;
+
+const ModalButton = styled.button`
+  margin: 0 4px;
+  padding: 4px 12px;
+  border-radius: 4px;
+  font-size: 14px;
+  transition: 0.3s;
+  &:hover {
+    transform: translateY(-4px);
+  }
+`;
+
+export const ModalCancel = styled(ModalButton)`
+  color: #242424;
+  background: #fbfbfb;
+  border: 1px solid #dddddd;
+`;
+
+export const ModalAdd = styled(ModalButton)`
+  color: white;
+  background: #5323b2;
+  border: 1px solid #5323b2;
 `;
