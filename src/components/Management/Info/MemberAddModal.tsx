840b7fc8ba4bc44f63f985f2e29c69f5db65a032
이성진 <chlskatnr1@naver.com>
2020-10-21 21:58:01 +0900

    [MODIFY] <MemberAddModal.tsx> 동아리 인원 추가 모달 컴포넌트 완성

--- src/components/Management/Info/MemberAddModal.tsx
+++ src/components/Management/Info/MemberAddModal.tsx
@@ -1,5 +1,11 @@
-import React, { FC, ReactElement, useState } from "react";
-import axios from "axios";
+import React, {
+  ChangeEvent,
+  FC,
+  ReactElement,
+  useCallback,
+  useRef,
+  useState
+} from "react";
 
 import * as S from "./style";
 
@@ -9,19 +15,21 @@ interface Props {
   handleCloseModal: () => void;
 }
 
-const users: string[] = [
-  "1101 홍길동",
-  "1102 홍길동",
-  "1103 홍길동",
-  "1104 홍길동",
-  "1105 홍길동",
-  "1106 홍길동",
-  "1107 홍길동",
-  "1108 홍길동"
+const initUsers: string[] = [
+  "1101 홍길동1",
+  "1102 홍길동2",
+  "1103 홍길동3",
+  "1104 홍길동4",
+  "1105 홍길동5",
+  "1106 홍길동6",
+  "1107 홍길동7",
+  "1108 홍길동8"
 ];
 
 const MemberAddModal: FC<Props> = ({ handleCloseModal }): ReactElement => {
-  const [queue, setQueue] = useState<string[]>([]);
+  const scrollWrap = useRef<HTMLUListElement>(null);
+  const [searchQueue, setSearchQueue] = useState<string[]>([]);
+  const [addQueue, setAddResult] = useState<string[]>([]);
 
   const addMembers = async () => {
     // const url = "http://www.naver.com";
@@ -31,29 +39,74 @@ const MemberAddModal: FC<Props> = ({ handleCloseModal }): ReactElement => {
     handleCloseModal();
   };
 
-  React.useEffect(() => {
-    setQueue(["2115 이성진", "2115 이성진"]);
+  const handleChangeFindUser = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    if (value === "") {
+      setSearchQueue([]);
+      return;
+    }
+
+    setSearchQueue(initUsers.filter(user => user.match(value)));
+  };
+
+  const handleClickAddQueue = useCallback((user: string) => {
+    setSearchQueue(prev => prev.filter(u => u !== user));
+    setAddResult(prev => [...prev, user]);
+  }, []);
+
+  const handleClickRemoveQueue = useCallback((user: string) => {
+    setAddResult(prev => prev.filter(u => u !== user));
+    setSearchQueue(prev => [...prev, user]);
   }, []);
 
   return (
     <S.Modal>
-      <div className="back" onClick={handleCloseModal} />
-      <div className="form">
-        <p>학생 검색</p>
-        <label>
-          <img src={Search} alt="search" title="search" />
-          <input type="text" placeholder="이름으로 검색하세요." />
-        </label>
-        <div className="queue">
-          {queue.map((q, i) => (
-            <div key={i}>{q}</div>
-          ))}
-        </div>
-        <div>
-          <button onClick={handleCloseModal}>취소</button>
-          <button onClick={addMembers}>추가</button>
-        </div>
-      </div>
+      <S.ModalBack onClick={handleCloseModal} />
+      <S.ModalForm>
+        <S.ModalFormInner>
+          <div>
+            <S.SearchingWrap>
+              <p>학생 검색</p>
+              <S.SearchLabel>
+                <img src={Search} alt="search" title="search" />
+                <S.SearchText
+                  onChange={handleChangeFindUser}
+                  type="text"
+                  placeholder="학번 또는 이름으로 검색하세요."
+                />
+              </S.SearchLabel>
+              <S.Result ref={scrollWrap}>
+                {searchQueue
+                  .sort((a, b) => (a > b ? 1 : -1))
+                  .map((r, i) => (
+                    <S.ResultItem
+                      key={`${i}_${r}`}
+                      onClick={() => handleClickAddQueue(r)}
+                    >
+                      <span>{i + 1}</span>
+                      <span>{r}</span>
+                    </S.ResultItem>
+                  ))}
+              </S.Result>
+            </S.SearchingWrap>
+          </div>
+          <S.Queue>
+            <p>추가 됨</p>
+            {addQueue
+              .sort((a, b) => (a > b ? 1 : -1))
+              .map((q, i) => (
+                <li key={i} onClick={() => handleClickRemoveQueue(q)}>
+                  {q}
+                </li>
+              ))}
+          </S.Queue>
+        </S.ModalFormInner>
+        <S.ModalButtonWrap>
+          <S.ModalCancel onClick={handleCloseModal}>취소</S.ModalCancel>
+          <S.ModalAdd onClick={addMembers}>추가</S.ModalAdd>
+        </S.ModalButtonWrap>
+      </S.ModalForm>
     </S.Modal>
   );
 };
