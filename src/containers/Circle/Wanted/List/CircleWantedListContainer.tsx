8a8430d5d0843b9e57227b4ed2d9a6f49f6b877e
YeongG <yeonggil223@gmail.com>
2020-12-04 23:44:47 +0900

    [MODIFY] Type변화에 따른 제네릭, Props변경

--- src/containers/Circle/Wanted/List/CircleWantedListContainer.tsx
+++ src/containers/Circle/Wanted/List/CircleWantedListContainer.tsx
@@ -1,48 +1,27 @@
-import React, { FC } from 'react';
-import { WantedCircleBoxData } from '../../../../components/default/CircleBox/WantedCircleBox';
-import { CircleWantedList } from '../../../../components';
-import { useEffect } from 'react';
-import { updatePosterListWanted } from '../../../../modules/action/poster';
-import { useDispatch } from 'react-redux';
+import React, { FC } from "react";
+import { CircleWantedList } from "../../../../components";
+import { useEffect } from "react";
+import { getWantedInfoList } from "../../../../modules/action/poster";
+import { useDispatch } from "react-redux";
+import { WantedInfo } from "../../../../modules/type/poster";
 
-const data: WantedCircleBoxData[] = [
+const data: WantedInfo[] = [
   {
-    name: 'VCC',
-    field: '임베디드',
-    description: '임베디드를 함께 공부할 열정있는 학생을 모집합니다!',
-    job: ['PCB 디자이너 1', 'Hardware 엔지니어 2'],
-    where: '세미나실 4-4',
-    grade: '2학년',
-    imgSrc:
-      'https://img.insight.co.kr/static/2018/09/12/700/z7n04ul8ig3y27w6l6ok.jpg',
-    date: '2020-07-25 ~ 08-01',
-  },
-  {
-    name: '동아리1',
-    field: '임베디드',
-    description: '저희는 동아리1 입니다!',
-    job: ['동아리1 일원', '동아리2 일원'],
-    where: '세미나실 1-1',
-    grade: '1학년',
-    imgSrc:
-      'https://img.insight.co.kr/static/2018/09/12/700/z7n04ul8ig3y27w6l6ok.jpg',
-  },
-  {
-    name: '동아리2',
-    field: '임베디드',
-    description: '저희는 동아리2 입니다!',
-    job: ['동아리2 일원', '동아리2 일원'],
-    where: '세미나실 2-2',
-    grade: '2학년',
-    imgSrc:
-      'https://img.insight.co.kr/static/2018/09/12/700/z7n04ul8ig3y27w6l6ok.jpg',
-  },
+    club_uuid: "클럽 uuid",
+    end_period: "2020-08-01",
+    start_period: "2020-07-25",
+    recruitment_uuid: "모집 uuid",
+    field: "모집 분야",
+    grade: 0,
+    number: 0,
+    recruit_concept: "모집 한줄 소개"
+  }
 ];
 
 const CircleWantedContainer: FC = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(updatePosterListWanted(data));
+    dispatch(getWantedInfoList(data));
   });
   return <CircleWantedList />;
 };
