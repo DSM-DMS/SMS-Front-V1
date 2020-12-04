87521d8c9418b5e74c500f31fed23f304a2d14b1
YeongG <yeonggil223@gmail.com>
2020-12-04 23:42:38 +0900

    [MODIFY] prettier로 인한 코드 변경

--- src/components/Circle/Notice/Detail/CircleNoticeDetail.tsx
+++ src/components/Circle/Notice/Detail/CircleNoticeDetail.tsx
@@ -1,12 +1,12 @@
-import React, { FC } from 'react';
-import { DetailPageHeader } from '../../../../components/default';
-import { NavIconNoticeBlue } from '../../../../assets';
-import * as S from './styles';
-import NoticeDetailBody from '../../../../components/Notice/Detail/NoticeDetailBody/NoticeDetailBody';
-import { customSelector } from '../../../../lib/api';
+import React, { FC } from "react";
+import { DetailPageHeader } from "../../../../components/default";
+import { NavIconNoticeBlue } from "../../../../assets";
+import * as S from "./styles";
+import NoticeDetailBody from "../../../../components/Notice/Detail/NoticeDetailBody/NoticeDetailBody";
+import { customSelector } from "../../../../lib/api";
 
 const CircleNoticeDetailContainer: FC = () => {
-  const { content } = customSelector((state) => state.board.detail);
+  const { content } = customSelector(state => state.board.detail);
 
   return (
     <S.Container>
--- src/containers/Notice/List/NoticeListContainer.tsx
+++ src/containers/Notice/List/NoticeListContainer.tsx
@@ -1,42 +1,41 @@
-import React, { FC } from 'react';
-import { NoticeList } from '../../../components';
-import { BoardObj } from '../../../components/default/Board/Board';
-import { useEffect } from 'react';
-import { updateBoardList } from '../../../modules/action/board';
-import { useDispatch } from 'react-redux';
-import { customSelector } from '../../../lib/api';
+import React, { FC } from "react";
+import { NoticeList } from "../../../components";
+import { BoardObj } from "../../../components/default/Board/Board";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getNoticeList } from "../../../modules/action/notice";
 
 const StaticData: BoardObj[] = [
   {
     id: 1,
-    title: '제목1',
-    date: '2020.07.08',
-    viewCount: 1,
+    title: "제목1",
+    date: "2020.07.08",
+    viewCount: 1
   },
   {
     id: 2,
-    title: '제목22',
-    date: '2020.07.08',
-    viewCount: 2,
+    title: "제목22",
+    date: "2020.07.08",
+    viewCount: 2
   },
   {
     id: 3,
-    title: '제목333',
-    date: '2020.07.08',
-    viewCount: 3,
+    title: "제목333",
+    date: "2020.07.08",
+    viewCount: 3
   },
   {
     id: 4,
-    title: '제목4444',
-    date: '2020.07.08',
-    viewCount: 4,
-  },
+    title: "제목4444",
+    date: "2020.07.08",
+    viewCount: 4
+  }
 ];
 
 const NoticeListContainer: FC = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(updateBoardList(StaticData));
+    dispatch(getNoticeList(StaticData));
   }, []);
   return <NoticeList />;
 };
