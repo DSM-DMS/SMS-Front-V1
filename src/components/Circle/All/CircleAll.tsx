fde30fc7110d4054dc12fefd68e9b2bfd589933d
YeongG <yeonggil223@gmail.com>
2020-12-04 23:40:25 +0900

    [MODIFY] customSelector => useSelector

--- src/components/Circle/All/CircleAll.tsx
+++ src/components/Circle/All/CircleAll.tsx
@@ -1,17 +1,18 @@
-import React, { FC, useState, useCallback, ChangeEvent, memo } from 'react';
-import * as S from '../Wanted/List/styles';
-import { PageHeader, Category, AllCircleBox } from '../../default';
-import { NavIconAllBlue } from '../../../assets';
-import { Hr } from '../../../components/default/Board/styles';
-import { AllCircleBoxType } from '../../default/CircleBox/AllCircleBox';
-import { makeFilterFunc, customSelector } from '../../../lib/api';
+import React, { FC, useState, useCallback, ChangeEvent, memo } from "react";
+import * as S from "../Wanted/List/styles";
+import { PageHeader, Category, AllCircleBox } from "../../default";
+import { NavIconAllBlue } from "../../../assets";
+import { Hr } from "../../../components/default/Board/styles";
+import { makeFilterFunc } from "../../../lib/api";
+import { useSelector } from "react-redux";
+import { stateType } from "../../../modules/reducer";
+import { CircleInfo } from "../../../modules/type/poster";
 
 const CircleAll: FC = () => {
-  const data = customSelector((state) => state.poster.all.list);
-  const [keyword, setkeyword] = useState<string>('');
-  const filterFunc = makeFilterFunc<AllCircleBoxType>(
-    data,
-    ({ name }, keyword) => name.includes(keyword),
+  const data = useSelector((store: stateType) => store.poster.all.list);
+  const [keyword, setkeyword] = useState<string>("");
+  const filterFunc = makeFilterFunc<CircleInfo>(data, ({ name }, keyword) =>
+    name.includes(keyword)
   );
   const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setkeyword(e.target.value);
@@ -30,18 +31,9 @@ const CircleAll: FC = () => {
         placeHolder="검색할 동아리 이름을 입력하세요"
       />
       <S.BoxWrap>
-        {filterFunc(keyword).map(
-          ({ name, leader, description, field, imgSrc, where }) => (
-            <AllCircleBox
-              name={name}
-              description={description}
-              field={field}
-              leader={leader}
-              imgSrc={imgSrc}
-              where={where}
-            />
-          ),
-        )}
+        {filterFunc(keyword).map(data => (
+          <AllCircleBox key={data.club_uuid} {...data} />
+        ))}
       </S.BoxWrap>
     </S.Container>
   );
--- src/components/default/Info/Header/AllHeader.tsx
+++ src/components/default/Info/Header/AllHeader.tsx
@@ -1,14 +1,16 @@
-import React, { FC } from 'react';
-import * as S from './styles';
-import { Hr } from '../../Board/styles';
-import { customSelector } from '../../../../lib/api';
+import React, { FC } from "react";
+import * as S from "./styles";
+import { Hr } from "../../Board/styles";
+import { customSelector } from "../../../../lib/api";
+import { useSelector } from "react-redux";
+import { stateType } from "../../../../modules/reducer";
 
 const AllHeader: FC = () => {
-  const name = customSelector((state) => state.poster.all.detail.name);
+  const name = useSelector((state: stateType) => state.poster.all.detail.name);
   return (
     <>
       <S.Container>
-        <S.Title>{name}</S.Title>
+        <S.Title>동아리 이름</S.Title>
       </S.Container>
       <Hr />
     </>
