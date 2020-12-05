950a2df20f476cf720a3710c5a0341c2b9cd6791
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-05 23:24:56 +0900

    [MODIFY] <components/Header> style

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export const HeaderWrap = styled.header`
   display: flex;
@@ -9,12 +10,14 @@ export const HeaderWrap = styled.header`
   padding: 12px 0;
   border-bottom: 1px solid #dddddd;
   font-size: 14px;
-  > button {
-    margin-left: 16px;
-    border: 0;
-    color: #ff5555;
-    background-color: transparent;
-    font-size: 14px;
-    font-weight: bold;
-  }
+`;
+
+export const Logout = styled(Link)`
+  margin-left: 16px;
+  border: 0;
+  color: #ff5555;
+  background-color: transparent;
+  font-size: 14px;
+  font-weight: bold;
+  text-decoration: none;
 `;
