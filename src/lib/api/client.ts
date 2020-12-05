18fd2ba1235f2ba445e2e08f7bb3eeb7a771523d
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-05 23:22:43 +0900

    [MODIFY] <lib/api/client.ts> expiration time setting

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,25 +1,35 @@
 import axios from "axios";
 
-export const BASE_URL = "http://10.220.158.111:8080/";
-export const VERSION = "v1";
+const SERVER = {
+  hostUrl: "http://10.220.158.111:8080/",
+  version: "v1"
+};
+
+const SESSION_EXPIRATION_MESSAGE =
+  "시간이 경과되어 세션이 만료되었습니다. 재로그인 후 다시 실행해주시기 바랍니다.";
+
+export const BASE_URL = `${SERVER.hostUrl}${SERVER.version}`;
 
 export const apiDefault = () => {
   const accessToken = localStorage.getItem("access_token");
   const expiration = localStorage.getItem("expiration");
 
   if (expiration) {
-    if (new Date(expiration).getTime() < new Date().getTime()) {
+    const now = new Date().getTime(),
+      expTime = new Date(expiration).getTime();
+
+    if (expTime < now) {
       localStorage.removeItem("access_token");
       localStorage.removeItem("student_uuid");
       localStorage.removeItem("expiration");
 
-      alert("");
+      alert(SESSION_EXPIRATION_MESSAGE);
       window.location.replace("/login");
     }
   }
 
   return axios.create({
-    baseURL: `${BASE_URL}${VERSION}`,
+    baseURL: `${BASE_URL}`,
     headers: {
       Authorization: `Bearer ${accessToken}`
     }
