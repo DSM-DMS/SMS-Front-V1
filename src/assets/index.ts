5ce9a5c03aa793cd8eb3a1e965bf4243b78e6525
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-07 22:19:41 +0900

    [ADD] <assets/Outing> outing place search svg

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -32,33 +32,41 @@ export { default as NavIconScheduleWhite } from "./navigation/NavIcon-schedule-w
 export { default as NavIconScheduleMint } from "./navigation/NavIcon-schedule-mint.svg";
 export { default as NavIconUnauthorizedBlack } from "./navigation/NavIcon-unAuthorized-black.svg";
 export { default as NavIconUnauthorizedMint } from "./navigation/NavIcon-unAuthorized-mint.svg";
+export { default as NavIconExitBlack } from "./navigation/NavIcon-exit-black.svg";
+export { default as NavIconTrashCanYellow } from "./navigation/NavIcon-trashCan-yellow.svg";
 export { default as IconWhite } from "./navigation/Icon-white.svg";
 export { default as IconBlack } from "./navigation/Icon-black.svg";
 export { default as CloseMenu } from "./navigation/CloseMenu.svg";
-export { default as SearchIcon } from "./Board/SearchIcon.svg";
+
 export { default as MainArrow } from "./Main/arrow.svg";
 export { default as MainBike } from "./Main/bike.svg";
 export { default as MainHistory } from "./Main/outingHistory.svg";
 export { default as MainWarning } from "./Main/warning.svg";
+
+export { default as SearchIcon } from "./Board/SearchIcon.svg";
+
 export { default as MainSelector } from "./Admin/selector.svg";
 export { default as MainChangeTable } from "./Admin/change.svg";
-export { default as OutingWarning } from "./Outing/warning.svg";
+
 export { default as OutingWarningRedBase } from "./Outing/warningRedBase.svg";
-export { default as OutingBike } from "./Outing/bike.svg";
 export { default as OutingDown } from "./Outing/dropDownButton.svg";
+export { default as OutingTriangle } from "./Outing/triangle.svg";
+export { default as OutingBalloons } from "./Outing/balloons.svg";
+export { default as OutingWarning } from "./Outing/warning.svg";
+export { default as OutingHistory } from "./Outing/history.svg";
+export { default as ModalClose } from "./Outing/modalClose.svg";
+export { default as OutingPlaceSearch } from "./Outing/search.svg";
+export { default as OutingClose } from "./Outing/close.svg";
+export { default as OutingBike } from "./Outing/bike.svg";
+export { default as OutingUser } from "./Outing/user.svg";
+
 export { default as FacebookIcon } from "./Circle/facebookIcon.svg";
 export { default as ReComment } from "./Circle/reComment.svg";
-export { default as OutingHistory } from "./Outing/history.svg";
-export { default as OutingTriangle } from "./Outing/triangle.svg";
-export { default as OutingClose } from "./Outing/close.svg";
-export { default as OutingBalloons } from "./Outing/balloons.svg";
-export { default as OutingUser } from "./Outing/user.svg";
-export { default as ModalClose } from "./Outing/modalClose.svg";
+
 export { default as calanderSvg } from "./management/calander.svg";
 export { default as plusMember } from "./management/plus.svg";
 export { default as deleteMember } from "./management/delete.svg";
 export { default as paperclipClubPicture } from "./management/paperclip.svg";
 export { default as Search } from "./management/search.svg";
-export { default as NavIconExitBlack } from "./navigation/NavIcon-exit-black.svg";
-export { default as NavIconTrashCanYellow } from "./navigation/NavIcon-trashCan-yellow.svg";
+
 export { default as PageNotFound } from "./default/page_not_found.svg";
