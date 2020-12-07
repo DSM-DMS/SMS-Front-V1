c59150b833ddfc768996490d8c5cea7cb949a0cb
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-07 22:44:18 +0900

    [ADD] <lib/api/Outing> 외출 API 추가

---
+++ src/lib/api/Outing/index.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+import { apiDefault } from "../client";
+import {
+  ResLocationWithDefault,
+  ReqOuting,
+  ResOutingWithDefault
+} from "../payloads/Outing";
+
+export const getNaverLocation = (query: string) => {
+  const encodingUrl = window.encodeURI(query);
+  return axios.get<ResLocationWithDefault>(
+    `https://openapi.naver.com/v1/search/local.json?query=${encodingUrl}&display=10&start=1&sort=random`,
+    {
+      headers: {
+        "X-Naver-Client-Id": process.env.NAVER_CLIENT_ID,
+        "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET
+      }
+    }
+  );
+};
+
+export const postOuting = (body: ReqOuting) => {
+  const { end_time, place, reason, situation, start_time } = body;
+  return apiDefault().post<ResOutingWithDefault>(`/outings`, {
+    start_time,
+    end_time,
+    place,
+    reason,
+    situation
+  });
+};
