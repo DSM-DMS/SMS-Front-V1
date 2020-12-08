a7e1b2bd8e8d722fbcce0e52b3ad8483ac6adbc9
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-08 20:04:48 +0900

    [MODIFY] <containers/Outing/HistoryContainer> 외출 내역 받아오기

--- src/containers/Outing/HistoryContainer.tsx
+++ src/containers/Outing/HistoryContainer.tsx
@@ -1,11 +1,45 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useEffect, useState } from "react";
 
-import { OutingHistory } from '../../components';
+import { OutingHistory } from "../../components";
+import { getHistory } from "../../lib/api/Outing";
+import { ResHistory } from "../../lib/api/payloads/Outing";
 
 interface Props {}
 
 const HistoryContainer: FC<Props> = (): ReactElement => {
-  return <OutingHistory />;
+  const [histories, setHistories] = useState<ResHistory>({
+    outings: []
+  });
+  const [modal, setModal] = useState<boolean>(false);
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  const openModal = () => {
+    setModal(true);
+  };
+
+  const getHistories = async () => {
+    const {
+      data: { outings }
+    } = await getHistory(localStorage.getItem("student_uuid"));
+
+    setHistories({ outings });
+  };
+
+  useEffect(() => {
+    getHistories();
+  }, []);
+
+  return (
+    <OutingHistory
+      histories={histories}
+      modal={modal}
+      closeModal={closeModal}
+      openModal={openModal}
+    />
+  );
 };
 
 export default HistoryContainer;
