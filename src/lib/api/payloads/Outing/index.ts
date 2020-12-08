b71698891630a4e3ba49ecf313e854dc05874656
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-08 19:58:13 +0900

    [MODIFY] 외출 신청 내역 response, 외출 상태 enum 추가

--- src/lib/api/payloads/Outing/index.ts
+++ src/lib/api/payloads/Outing/index.ts
@@ -1,5 +1,16 @@
 import { ResDefault } from "../";
 
+export enum OutingStatus {
+  "선생님 거절" = -2,
+  "학부모 거절" = -1,
+  "외출증 신청" = 0,
+  "학부모 승인 " = 1,
+  "선생님 승인 " = 2,
+  "외출 시작" = 3,
+  "외출 종료" = 4,
+  "외출 인증 승인" = 5
+}
+
 export interface ResLocationItem {
   title: string;
   link: string;
@@ -32,5 +43,20 @@ export interface ResOuting {
   outing_uuid: string;
 }
 
+export interface ResHistoryItem {
+  end_time: number;
+  outing_situation: string;
+  outing_status: string;
+  outing_uuid: string;
+  place: string;
+  reason: string;
+  start_time: number;
+}
+
+export interface ResHistory {
+  outings: ResHistoryItem[];
+}
+
 export interface ResLocationWithDefault extends ResLocation, ResDefault {}
 export interface ResOutingWithDefault extends ResOuting, ResDefault {}
+export interface ResHistoryWithDefault extends ResHistory, ResDefault {}
