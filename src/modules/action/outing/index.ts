8ccd01a0b67e473262291ed6c9afb7e5ff43b576
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-09 10:55:17 +0900

    [MODIFY] <modules/action/outing> action naming 변경

--- src/modules/action/outing/index.ts
+++ src/modules/action/outing/index.ts
@@ -3,15 +3,15 @@ import { ResHistoryItem } from "../../../lib/api/payloads/Outing";
 export const SET_OUTING_LIST = "outing/SET_OUTING_LIST" as const;
 export const SET_SELECTED_OUTING = "outing/SET_SELECTED_OUTING" as const;
 
-export const setOutingList = (outings: ResHistoryItem[]) => ({
+export const setOutingHistoryList = (histories: ResHistoryItem[]) => ({
   type: SET_OUTING_LIST,
-  payload: { outings }
+  payload: { histories }
 });
-export const setSelectedOuting = (outing: ResHistoryItem) => ({
+export const setSelectedHistory = (history: ResHistoryItem) => ({
   type: SET_SELECTED_OUTING,
-  payload: { outing }
+  payload: { history }
 });
 
 export type OutingAction = ReturnType<
-  typeof setOutingList | typeof setSelectedOuting
+  typeof setOutingHistoryList | typeof setSelectedHistory
 >;
