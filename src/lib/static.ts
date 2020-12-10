73d175d1ce96ba362b8b32a550a686ebe79ac7bd
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-10 11:26:18 +0900

    [MODIFY] <lib/static> 네비게이션 외출 waring -> warning 수정

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -22,12 +22,12 @@ import {
   NavIconCircleMint,
   NavIconUnauthorizedBlack,
   NavIconUnauthorizedMint,
-  NavIconHomeBlack,
-} from '../assets';
+  NavIconHomeBlack
+} from "../assets";
 import {
   MainSubItem,
-  NavItem,
-} from '../components/Navigation/Main/body/NavigationBody';
+  NavItem
+} from "../components/Navigation/Main/body/NavigationBody";
 
 export interface RouteData {
   main: NavItem[];
@@ -39,89 +39,89 @@ export interface RouteData {
 export const userRoute: RouteData = {
   main: [
     {
-      name: '홈',
-      route: '/home',
+      name: "홈",
+      route: "/home",
       white: NavIconHomeWhite,
-      blue: NavIconHomeBlue,
+      blue: NavIconHomeBlue
     },
     {
-      name: '공지',
-      route: '/notice',
+      name: "공지",
+      route: "/notice",
       white: NavIconNoticeWhite,
-      blue: NavIconNoticeBlue,
-    },
+      blue: NavIconNoticeBlue
+    }
   ],
   sub: [
     {
-      name: '동아리',
-      subUrl: '공지사항',
-      route: '/circles/notice',
+      name: "동아리",
+      subUrl: "공지사항",
+      route: "/circles/notice",
       white: NavIconCircleWhite,
-      blue: NavIconCircleBlue,
+      blue: NavIconCircleBlue
     },
     {
-      name: '외출신청',
-      subUrl: '유의사항',
-      route: '/outing/waring',
+      name: "외출신청",
+      subUrl: "유의사항",
+      route: "/outing/waring",
       white: NavIconOutingWhite,
-      blue: NavIconOutingBlue,
-    },
+      blue: NavIconOutingBlue
+    }
   ],
-  color: '#5323B2',
+  color: "#5323B2"
 };
 
 export const adminRouter: RouteData = {
   main: [
     {
-      name: '학사 일정',
-      route: '/admin/home',
+      name: "학사 일정",
+      route: "/admin/home",
       white: NavIconScheduleWhite,
-      blue: NavIconScheduleMint,
-    },
+      blue: NavIconScheduleMint
+    }
   ],
   sub: [
     {
-      name: '외출 관리',
-      route: '/admin/out/wait',
-      subUrl: '승인대기 외출증',
+      name: "외출 관리",
+      route: "/admin/out/wait",
+      subUrl: "승인대기 외출증",
       white: NavIconOutingWhite,
-      blue: NavIconOutingMint,
+      blue: NavIconOutingMint
     },
     {
-      name: '공지사항',
-      subUrl: '전체 공지',
-      route: '/admin/notice/all',
+      name: "공지사항",
+      subUrl: "전체 공지",
+      route: "/admin/notice/all",
       white: NavIconNoticeWhite,
-      blue: NavIconNoticeMint,
-    },
+      blue: NavIconNoticeMint
+    }
   ],
-  color: '#23B2AD',
+  color: "#23B2AD"
 };
 
 export const managementRouter: RouteData = {
   main: [
     {
-      name: '정보수정',
-      route: '/management/edit',
+      name: "정보수정",
+      route: "/management/edit",
       white: NavIconHomeBlack,
-      blue: NavIconHomeBlack,
+      blue: NavIconHomeBlack
     },
     {
-      name: '모집관리',
-      route: '/management/wanted',
+      name: "모집관리",
+      route: "/management/wanted",
       white: NavIconCircleBlack,
-      blue: NavIconCircleBlack,
+      blue: NavIconCircleBlack
     },
     {
-      name: '공지 관리',
-      route: '/management/notice',
+      name: "공지 관리",
+      route: "/management/notice",
       white: NavIconNoticeBlack,
-      blue: NavIconNoticeBlack,
-    },
+      blue: NavIconNoticeBlack
+    }
   ],
   sub: [],
-  color: '#FFFFFF',
-  isManagementMode: true,
+  color: "#FFFFFF",
+  isManagementMode: true
 };
 
 interface SubNavItem {
@@ -134,91 +134,91 @@ interface SubNavItem {
 export type SubNavObj = {
   동아리: SubNavItem[];
   외출신청: SubNavItem[];
-  '외출 관리': SubNavItem[];
+  "외출 관리": SubNavItem[];
   공지사항: SubNavItem[];
 };
 
-export type PageType = '동아리' | '외출신청' | '외출 관리' | '공지사항';
+export type PageType = "동아리" | "외출신청" | "외출 관리" | "공지사항";
 
 export const subNavRouter: SubNavObj = {
   동아리: [
     {
-      name: '공지사항',
+      name: "공지사항",
       url: NavIconNoticeBlack,
       acitveUrl: NavIconNoticeBlue,
-      route: '/circles/notice',
+      route: "/circles/notice"
     },
     {
-      name: '부원 모집',
+      name: "부원 모집",
       url: NavIconCircleBlack,
       acitveUrl: NavIconCircleBlue,
-      route: '/circles/wanted',
+      route: "/circles/wanted"
     },
     {
-      name: '동아리 전체보기',
+      name: "동아리 전체보기",
       url: NavIconAllBlack,
       acitveUrl: NavIconAllBlue,
-      route: '/circles/all',
-    },
+      route: "/circles/all"
+    }
   ],
   외출신청: [
     {
-      name: '유의사항',
+      name: "유의사항",
       url: NavIconWaringBlack,
       acitveUrl: NavIconWaringRed,
-      route: '/outing/waring',
+      route: "/outing/warning"
     },
     {
-      name: '외출신청',
+      name: "외출신청",
       url: NavIconOutingBlack,
       acitveUrl: NavIconOutingBlue,
-      route: '/outing/apply',
+      route: "/outing/apply"
     },
     {
-      name: '내 외출신청 내역',
+      name: "내 외출신청 내역",
       url: NavIconHistoryBlack,
       acitveUrl: NavIconHistoryBlack,
-      route: '/outing/history',
-    },
+      route: "/outing/history"
+    }
   ],
-  '외출 관리': [
+  "외출 관리": [
     {
-      name: '승인대기 외출증',
+      name: "승인대기 외출증",
       url: NavIconOutingBlack,
       acitveUrl: NavIconOutingMint,
-      route: '/admin/out/wait',
+      route: "/admin/out/wait"
     },
     {
-      name: '현재 외출 학생',
+      name: "현재 외출 학생",
       url: NavIconCircleBlack,
-      route: '/admin/out/now',
-      acitveUrl: NavIconCircleMint,
+      route: "/admin/out/now",
+      acitveUrl: NavIconCircleMint
     },
     {
-      name: '미인증 외출증',
+      name: "미인증 외출증",
       url: NavIconUnauthorizedBlack,
-      route: '/admin/out/certified',
-      acitveUrl: NavIconUnauthorizedMint,
-    },
+      route: "/admin/out/certified",
+      acitveUrl: NavIconUnauthorizedMint
+    }
   ],
   공지사항: [
     {
-      name: '전체 공지',
+      name: "전체 공지",
       url: NavIconAllBlack,
-      acitveUrl: '',
-      route: '/admin/notice/all',
+      acitveUrl: "",
+      route: "/admin/notice/all"
     },
     {
-      name: '내가 올린 공지',
+      name: "내가 올린 공지",
       url: NavIconWaringBlack,
-      acitveUrl: '',
-      route: '/admin/notice/mine',
+      acitveUrl: "",
+      route: "/admin/notice/mine"
     },
     {
-      name: '공지사항 작성',
-      url: '',
-      acitveUrl: '',
-      route: '/admin/notice/writing',
-    },
-  ],
+      name: "공지사항 작성",
+      url: "",
+      acitveUrl: "",
+      route: "/admin/notice/writing"
+    }
+  ]
 };
