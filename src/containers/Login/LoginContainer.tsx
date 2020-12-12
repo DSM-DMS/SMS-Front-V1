f4264cd6a13533d881e257f77d1976b922eebc2e
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-12 14:04:05 +0900

    [MODIFY] <containers/LoginContainer> 로그인 성공 시 시간표 불러오기

--- src/containers/Login/LoginContainer.tsx
+++ src/containers/Login/LoginContainer.tsx
@@ -22,6 +22,7 @@ import {
   TEACHER,
   UserType
 } from "../../modules/action/header";
+import { setTimetablesSaga } from "../../modules/action/main";
 import { pageMove } from "../../modules/action/page";
 
 interface Props {}
@@ -188,6 +189,7 @@ const LoginContainer: FC<Props> = () => {
         setErrorMessage(initErrorState);
 
         dispatch(pageMove("홈"));
+        dispatch(setTimetablesSaga());
         history.push("./home");
       } catch (err) {
         const data = err?.response?.data,
