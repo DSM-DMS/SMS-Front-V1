e8eedf0c70381750bbdce5fca67726b9e27b424b
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-14 08:58:38 +0900

    [MODIFY] <modules/outing> 외출증 내역 리셋 타입 추가

--- src/modules/action/outing/index.ts
+++ src/modules/action/outing/index.ts
@@ -1,8 +1,12 @@
 import { ResHistoryItem } from "../../../lib/api/payloads/Outing";
 
+export const RESET_OUTING_LIST = "outing/RESET_OUTING_LIST" as const;
 export const SET_OUTING_LIST = "outing/SET_OUTING_LIST" as const;
 export const SET_SELECTED_OUTING = "outing/SET_SELECTED_OUTING" as const;
 
+export const resetOutingHistoryList = () => ({
+  type: RESET_OUTING_LIST
+});
 export const setOutingHistoryList = (histories: ResHistoryItem[]) => ({
   type: SET_OUTING_LIST,
   payload: { histories }
@@ -13,5 +17,7 @@ export const setSelectedHistory = (history: ResHistoryItem) => ({
 });
 
 export type OutingAction = ReturnType<
-  typeof setOutingHistoryList | typeof setSelectedHistory
+  | typeof resetOutingHistoryList
+  | typeof setOutingHistoryList
+  | typeof setSelectedHistory
 >;
