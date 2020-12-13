dc5f1a1763a5780a26fd8b44ca7b18f828c22980
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-13 21:44:58 +0900

    [REMOVE] 사용하지 않거나 필요없는 코드 삭제

--- src/lib/api/Main/index.ts
+++
@@ -1,8 +0,0 @@
-import { apiDefault } from "../client";
-import { ResTimetableWithDefault } from "../payloads/Main";
-
-export const getTimetable = (weekNum: number) => {
-  return apiDefault().get<ResTimetableWithDefault>(
-    `time-tables/week-numbers/${weekNum}`
-  );
-};
