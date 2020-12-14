0c924ec16e90e4ba69ed73c89b7b8bdee73589b4
YeongG <yeonggil223@gmail.com>
2020-12-14 23:28:33 +0900

    [MODIFY] 랜더링시 dispatch

--- src/components/Notice/List/NoticeList.tsx
+++ src/components/Notice/List/NoticeList.tsx
@@ -1,23 +1,37 @@
-import React, { FC, useState, useCallback, ChangeEvent } from "react";
+import React, {
+  FC,
+  useState,
+  useCallback,
+  ChangeEvent,
+  useEffect
+} from "react";
 import { NavIconNoticeBlue } from "../../../assets";
 import { Board, ListPageHeader } from "../../default";
 import * as S from "./styles";
 import { makeFilterFunc } from "../../../lib/utils";
 import { BoardObj } from "../../default/Board/Board";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { stateType } from "../../../modules/reducer";
+import { BoardListitem } from "../../../modules/type/board";
+import { getNoticeListSaga } from "../../../modules/action/notice";
 
 const names = ["번호", "제목", "날짜", "조회수"];
 
 const NoticeContainer: FC = () => {
+  const dispatch = useDispatch();
+  const [index, setIndex] = useState<number>(0);
   const data = useSelector((state: stateType) => state.notice.list);
-  const noticeFilterFunc = makeFilterFunc<BoardObj>(
+  const noticeFilterFunc = makeFilterFunc<BoardListitem>(
     data,
     ({ title }, keyword) => title.includes(keyword)
   );
   const [keyword, setKeyword] = useState("");
   const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setKeyword(e.target.value);
+  }, []);
+
+  useEffect(() => {
+    dispatch(getNoticeListSaga(index));
   }, []);
 
   return (
