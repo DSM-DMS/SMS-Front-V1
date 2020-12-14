e65c662881e9fc7f483f341823eeffe39f39e265
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-14 09:00:19 +0900

    [MODIFY] <components/Main/ScheduelDetail> 세부내용 렌더링 완료

--- src/components/Main/ScheduleDetail/ScheduleDetail.tsx
+++ src/components/Main/ScheduleDetail/ScheduleDetail.tsx
@@ -1,28 +1,43 @@
-import React, { FC, ReactElement } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React, { FC, ReactElement } from "react";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import { ModalType } from '../../Admin/Main/Main';
-import * as S from '../style';
-import { stateType } from '../../../modules/reducer';
+import * as S from "../style";
+import { ModalType } from "../../Admin/Main/Main";
+import { stateType } from "../../../modules/reducer";
 
 interface Props {
   handleClickShowModal?: (type: ModalType) => void;
 }
 
 const ScheduleDetail: FC<Props> = ({ handleClickShowModal }): ReactElement => {
-  const { schedules } = useSelector((state: stateType) => state.scheduleDetail);
+  const { schedules } = useSelector((state: stateType) => state.main);
   const location = useLocation();
+
+  const handleShowEdit = () => {
+    handleClickShowModal("edit");
+  };
+
+  const handleShowDelete = () => {
+    handleClickShowModal("delete");
+  };
+
+  const getLocalDate = (dateNum: number) => {
+    const date = new Date(dateNum);
+    const padNum = (n: number) => (n < 10 ? `0${n}` : n + "");
+
+    return `${padNum(date.getMonth() + 1)}.${padNum(date.getDate())}`;
+  };
 
   return (
     <S.ScheduleDetail>
       <S.DetailHeader>
         <S.DetailHeaderTop>
           <S.DetailTitle>세부내용</S.DetailTitle>
-          {location.pathname.split('/')[1] === 'admin' && (
+          {location.pathname.includes("admin") && (
             <S.DetailAddSchedule
               onClick={() => {
-                handleClickShowModal('add');
+                handleClickShowModal("add");
               }}
             >
               <span>일정 추가</span>
@@ -35,26 +50,20 @@ const ScheduleDetail: FC<Props> = ({ handleClickShowModal }): ReactElement => {
         </S.DetailHead>
       </S.DetailHeader>
       <S.DetailBody>
-        {schedules.map(({ schedule, startDate, endDate }, i) => (
-          <S.DetailBodyItem key={i}>
-            <S.DetailBodyItemData>{schedule}</S.DetailBodyItemData>
+        {schedules.map(({ detail, start_date, end_date, schedule_uuid }) => (
+          <S.DetailBodyItem key={schedule_uuid}>
+            <S.DetailBodyItemData>{detail}</S.DetailBodyItemData>
             <S.DetailBodyItemData>
-              {startDate === endDate ? startDate : `${startDate} - ${endDate}`}
+              {start_date === end_date
+                ? getLocalDate(start_date)
+                : `${getLocalDate(start_date)} - ${getLocalDate(end_date)}`}
             </S.DetailBodyItemData>
-            {location.pathname.split('/')[1] === 'admin' && (
+            {location.pathname.includes("admin") && (
               <S.DetailBodyItemButtonWrap>
-                <S.DetailBodyItemButton
-                  onClick={() => {
-                    handleClickShowModal('edit');
-                  }}
-                >
+                <S.DetailBodyItemButton onClick={handleShowEdit}>
                   수정
                 </S.DetailBodyItemButton>
-                <S.DetailBodyItemButton
-                  onClick={() => {
-                    handleClickShowModal('delete');
-                  }}
-                >
+                <S.DetailBodyItemButton onClick={handleShowDelete}>
                   삭제
                 </S.DetailBodyItemButton>
               </S.DetailBodyItemButtonWrap>
