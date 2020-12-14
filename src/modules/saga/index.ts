8b4907f60f5a200d0fee4ab167f606299f62e960
YeongG <yeonggil223@gmail.com>
2020-12-14 23:26:52 +0900

    [MODIFY] redux saga index

--- src/modules/saga/index.ts
+++ src/modules/saga/index.ts
@@ -3,9 +3,10 @@ import { all } from "redux-saga/effects";
 import jsonSaga from "./json";
 import posterSaga from "./poster";
 import mainSaga from "./main";
+import noticeSaga from "./notion";
 
 function* rootSaga() {
-  yield all([jsonSaga(), posterSaga(), mainSaga()]);
+  yield all([jsonSaga(), posterSaga(), mainSaga(), noticeSaga()]);
 }
 
 export default rootSaga;
