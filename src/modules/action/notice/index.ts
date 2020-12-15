d344316d4c568bf4d7e635159a1d6d04c32e5007
YeongG <yeonggil223@gmail.com>
2020-12-15 23:35:46 +0900

    [MODIFY] redux notice action/reducer/saga/type

--- src/modules/action/notice/index.ts
+++ src/modules/action/notice/index.ts
@@ -8,6 +8,9 @@ export const GET_NOTICE_LIST_SAGA = "notice/GET_NOTICE_LIST_SAGA" as const;
 export const GET_NOTICE_DETAIL = "notice/GET_NOTICE_DETAIL" as const;
 export const GET_NOTICE_DETAIL_SAGA = "notice/GET_NOTICE_DETAIL_SAGA" as const;
 
+export const GET_CIRCLE_NOTICE_LIST_SAGA = "notice/GET_CIRCLE_NOTICE_LIST_SAGA" as const;
+export const GET_CIRCLE_NOTICE_DETAIL_SAGA = "notice/GET_CIRCLE_NOTICE_DETAIL_SAGA" as const;
+
 export const getNoticeList = (payload: BoardListitem[]) => ({
   type: GET_NOTICE_LIST,
   payload
@@ -20,8 +23,18 @@ export const getNoticeDetail = (payload: NoticeDetail) => ({
   type: GET_NOTICE_DETAIL,
   payload
 });
-export const getNoticeDetailSaga = () => ({
-  type: GET_NOTICE_DETAIL_SAGA
+export const getNoticeDetailSaga = (payload: string) => ({
+  type: GET_NOTICE_DETAIL_SAGA,
+  payload
+});
+
+export const getCircleNoticeDetailSaga = (payload: string) => ({
+  type: GET_CIRCLE_NOTICE_DETAIL_SAGA,
+  payload
+});
+export const getCircleNoticeListSaga = (payload: number) => ({
+  type: GET_CIRCLE_NOTICE_LIST_SAGA,
+  payload
 });
 
 export type NoticeAction = ReturnType<
@@ -29,4 +42,6 @@ export type NoticeAction = ReturnType<
   | typeof getNoticeListSaga
   | typeof getNoticeDetail
   | typeof getNoticeDetailSaga
+  | typeof getCircleNoticeDetailSaga
+  | typeof getCircleNoticeListSaga
 >;
