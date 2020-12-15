5ece6bcc03e9e28337649016b3d4ba9a750576a6
YeongG <yeonggil223@gmail.com>
2020-12-15 23:42:16 +0900

    [MODIFY] page받아와서 dispatch

--- src/containers/Notice/List/NoticeListContainer.tsx
+++ src/containers/Notice/List/NoticeListContainer.tsx
@@ -1,8 +1,16 @@
-import React, { FC } from "react";
-import { NoticeList } from "../../../components";
+import React, { FC, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { NoticeList } from "../../../components";
+import { getNoticeListSaga } from "../../../modules/action/notice";
+import qs from "query-string";
+import { RouteChildrenProps } from "react-router-dom";
 
-const NoticeListContainer: FC = () => {
+const NoticeListContainer: FC<RouteChildrenProps> = ({ location }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const page = qs.parse(location.search).page || 0;
+    dispatch(getNoticeListSaga(Number(page)));
+  }, []);
   return <NoticeList />;
 };
 
