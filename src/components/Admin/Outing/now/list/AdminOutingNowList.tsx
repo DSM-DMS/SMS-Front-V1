b616d27899147bb9e0fee83b8ee9c037e4fcf895
YeongG <yeonggil223@gmail.com>
2020-12-16 22:38:01 +0900

    [MODIFY] prettier로 인한 코드 변경

--- src/components/Admin/Outing/now/list/AdminOutingNowList.tsx
+++ src/components/Admin/Outing/now/list/AdminOutingNowList.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { FC } from 'react';
-import { OutingCardPage } from '../../../../default';
+import React from "react";
+import { FC } from "react";
+import { OutingCardPage } from "../../../../default";
 
 const AdminOutingNowList: FC = () => {
   return <OutingCardPage title="현재 외출 학생" isClicked={false} />;
