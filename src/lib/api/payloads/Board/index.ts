27ea3ee998368ec97eb5fe334329aebfccd2b494
YeongG <yeonggil223@gmail.com>
2020-12-16 23:09:46 +0900

    [CREATE] module/type/board => payload/board

---
+++ src/lib/api/payloads/Board/index.ts
@@ -0,0 +1,9 @@
+export interface BoardListItem {
+  announcement_uuid: string;
+  number: number;
+  title: string;
+  date: string;
+  views: number;
+  writer_name: string;
+  is_checked: number;
+}
