82207bb6df7155ee3b58f409041cb626dee0a4e3
YeongG <yeonggil223@gmail.com>
2020-12-16 23:09:20 +0900

    [MODIFY] 로직 변경으로 인한 코드 삭제

--- src/components/Admin/Outing/Wait/List/AdminOutingCardWaitList.tsx
+++ src/components/Admin/Outing/Wait/List/AdminOutingCardWaitList.tsx
@@ -7,19 +7,3 @@ const AdminOutingCardWaitList: FC = () => {
 };
 
 export default AdminOutingCardWaitList;
-
-type HOCWrap<T extends {}> = (a: T) => FC<T>;
-type HOC = <T extends {}>(a: FC<T>) => HOCWrap<T>;
-
-const IsLoginHOC: HOC = <T extends {}>(Component: FC<T>) => {
-  const component: HOCWrap<T> = (props: T) => {
-    return <Component {...props} />;
-  };
-  return component;
-};
-
-const A: FC<{ data: string }> = () => {
-  return <div>a</div>;
-};
-
-IsLoginHOC<{}>(A);
