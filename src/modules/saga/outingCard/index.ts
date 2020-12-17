c869f1d6ddbcbdcbf833e3ee802e5ad798dd525e
YeongG <yeonggil223@gmail.com>
2020-12-17 10:38:48 +0900

    [CRAETE] redux outingCard saga

---
+++ src/modules/saga/outingCard/index.ts
@@ -0,0 +1,30 @@
+import { AxiosResponse } from "axios";
+import { all, call, put, takeEvery } from "redux-saga/effects";
+import { apiDefault } from "../../../lib/api/client";
+import { getOutingCardList } from "../../../lib/api/OutingCard";
+import { ResOutingCardListItem } from "../../../lib/api/payloads/OutingCard";
+import {
+  getOutingCardListSaga as getOutingCardListSagaCreater,
+  GET_OUTING_CARD_LIST_SAGA,
+  getOutingCardList as getOutingCardListCreater
+} from "../../action/outingCard";
+
+function* getOutingCardListSaga(
+  action: ReturnType<typeof getOutingCardListSagaCreater>
+) {
+  try {
+    const res: AxiosResponse<{ outings: ResOutingCardListItem[] }> = yield call(
+      getOutingCardList,
+      action.payload
+    );
+    console.log(res.data);
+
+    yield put(getOutingCardListCreater(res.data.outings));
+  } catch (err) {}
+}
+
+function* outingCardSaga() {
+  yield takeEvery(GET_OUTING_CARD_LIST_SAGA, getOutingCardListSaga);
+}
+
+export default outingCardSaga;
