a1d50f5f588526003f864d89d9f8e853869421f0
YeongG <yeonggil223@gmail.com>
2020-12-17 10:53:22 +0900

    [MODIFY] redux outingCard action/reducer

--- src/modules/reducer/OutingCard/index.ts
+++ src/modules/reducer/OutingCard/index.ts
@@ -1,34 +1,47 @@
+import { ResOutingCardListItem } from "../../../lib/api/payloads/OutingCard";
 import {
+  CLOSE_OUTING_CARD_MODAL,
+  GET_OUTING_CARD_LIST,
   OutingCardAction,
+  SHOW_OUTING_CARD_MODAL,
   UPDATE_OUTING_CARD_LIST,
-  UPDATE_OUTING_CARD_MODAL,
-} from '../../action/outingCard';
-import { OutingCard } from '../../action/outingCard';
+  UPDATE_OUTING_CARD_MODAL
+} from "../../action/outingCard";
 
 interface OutingCardState {
-  list: OutingCard[];
-  id: number;
+  list: ResOutingCardListItem[];
+  outingUuid: string;
+  modalIsOpen: boolean;
 }
 const initialState: OutingCardState = {
   list: [],
-  id: 0,
+  outingUuid: "",
+  modalIsOpen: false
 };
 
 const OutingCardReducer = (
   state: OutingCardState = initialState,
-  action: OutingCardAction,
+  action: OutingCardAction
 ): OutingCardState => {
   switch (action.type) {
-    case UPDATE_OUTING_CARD_LIST: {
+    case GET_OUTING_CARD_LIST: {
       return {
         ...state,
-        list: action.payload,
+        list: action.payload
       };
     }
-    case UPDATE_OUTING_CARD_MODAL: {
+    case SHOW_OUTING_CARD_MODAL: {
       return {
         ...state,
-        id: action.payload,
+        outingUuid: action.payload,
+        modalIsOpen: true
+      };
+    }
+    case CLOSE_OUTING_CARD_MODAL: {
+      return {
+        ...state,
+        outingUuid: "",
+        modalIsOpen: false
       };
     }
     default: {
