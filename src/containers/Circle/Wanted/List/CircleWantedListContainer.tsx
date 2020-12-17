50fbaf7cfa823f6cbeff4fe3233af6bd20ffe734
YeongG <yeonggil223@gmail.com>
2020-12-17 10:52:53 +0900

    [MODIFY] 서버요청 2번 에러 해결

--- src/containers/Circle/Wanted/List/CircleWantedListContainer.tsx
+++ src/containers/Circle/Wanted/List/CircleWantedListContainer.tsx
@@ -1,18 +1,14 @@
 import React, { FC } from "react";
 import { CircleWantedList } from "../../../../components";
 import { useEffect } from "react";
-import {
-  getWantedInfoList,
-  getWantedInfoListSaga
-} from "../../../../modules/action/poster";
+import { getWantedInfoListSaga } from "../../../../modules/action/poster";
 import { useDispatch } from "react-redux";
-import { WantedInfo } from "../../../../modules/type/poster";
 
 const CircleWantedContainer: FC = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getWantedInfoListSaga());
-  });
+  }, []);
   return <CircleWantedList />;
 };
 
