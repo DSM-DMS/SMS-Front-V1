e0915576a8b3e2622fa352b3a0248c6caa1a2df0
YeongG <yeonggil223@gmail.com>
2020-12-18 23:48:28 +0900

    [MODIFY] 글쓰기 생성 type 생성

--- src/lib/api/payloads/Board/index.ts
+++ src/lib/api/payloads/Board/index.ts
@@ -7,3 +7,14 @@ export interface BoardListItem {
   writer_name: string;
   is_checked: number;
 }
+
+export interface BoardWriteFilter {
+  target_grade?: number;
+  target_group?: number;
+}
+
+export interface ReqBoardWriteFilter extends BoardWriteFilter {
+  type: string;
+  title: string;
+  content: string;
+}
