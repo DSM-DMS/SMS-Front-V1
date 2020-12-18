2b5eba3694b64eb4155e4c164d36bd9811d5e91b
YeongG <yeonggil223@gmail.com>
2020-12-18 23:45:05 +0900

    [MODIFY] status props 추가

--- src/components/Admin/Outing/now/list/AdminOutingNowList.tsx
+++ src/components/Admin/Outing/now/list/AdminOutingNowList.tsx
@@ -1,9 +1,15 @@
-import React from "react";
-import { FC } from "react";
+import React, { FC } from "react";
+import { OutingStatus } from "../../../../../lib/api/payloads/Outing";
 import { OutingCardPage } from "../../../../default";
 
 const AdminOutingNowList: FC = () => {
-  return <OutingCardPage title="현재 외출 학생" isClicked={false} />;
+  return (
+    <OutingCardPage
+      status={OutingStatus["외출 시작"]}
+      title="현재 외출 학생"
+      isClicked={false}
+    />
+  );
 };
 
 export default AdminOutingNowList;
