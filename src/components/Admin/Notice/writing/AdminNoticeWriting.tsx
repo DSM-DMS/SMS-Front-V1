1a2dfceafc18f56fdbc06de0827faad1fe4e2354
YeongG <yeonggil223@gmail.com>
2020-12-18 23:50:19 +0900

    [MODIFY] 글쓰기 필터 기능 추가

--- src/components/Admin/Notice/writing/AdminNoticeWriting.tsx
+++ src/components/Admin/Notice/writing/AdminNoticeWriting.tsx
@@ -1,16 +1,25 @@
-import React, { FC } from "react";
-import { useEffect } from "react";
+import React, {
+  FC,
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  ChangeEvent
+} from "react";
 import { NavIconNoticeMint } from "../../../../assets";
 import { PageHeader } from "../../../default";
 import * as S from "./styles";
 import EditerJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 import List from "@editorjs/list";
-import { useRef } from "react";
-import { useCallback } from "react";
+import WriteCategory from "../../../default/Category/WriteCategory";
+import { BoardWriteFilter } from "../../../../lib/api/payloads/Board";
 
 const AdminNoticeWriting: FC = () => {
   const editerRef = useRef<EditerJS>();
+  const [headeText, setHeaderText] = useState<string>();
+  const [filterData, setFilterData] = useState<BoardWriteFilter>({});
+
   useEffect(() => {
     const editer = new EditerJS({
       holder: "editer",
@@ -22,17 +31,31 @@ const AdminNoticeWriting: FC = () => {
     editerRef.current = editer;
   }, []);
 
+  const changeHeaderText = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setHeaderText(e.target.value);
+  }, []);
+
+  const writeFilterHandler = useCallback((data: BoardWriteFilter) => {
+    setFilterData(data);
+  }, []);
+
   const saveHandler = useCallback(async () => {
     if (!editerRef) return;
-    console.log(await editerRef.current.save());
-  }, []);
+    const writeData = await editerRef.current.save();
+  }, [filterData]);
 
   return (
     <S.Container>
       <PageHeader imgSrc={NavIconNoticeMint} title="공지사항" type="DETAIL" />
       <S.Hr />
-      <S.TitleInput type="text" placeholder="제목을 입력하세요" />
+      <S.TitleInput
+        onChange={changeHeaderText}
+        value={headeText}
+        type="text"
+        placeholder="제목을 입력하세요"
+      />
       <S.Hr />
+      <WriteCategory onChange={writeFilterHandler} />
       <S.EditerBackground>
         <S.Editer id="editer"></S.Editer>
       </S.EditerBackground>
