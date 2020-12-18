eae1eeafa541dbd06fad543ace8e570d8465027e
YeongG <yeonggil223@gmail.com>
2020-12-18 23:47:52 +0900

    [CREATE] redux write action/saga

---
+++ src/modules/action/write/index.ts
@@ -0,0 +1,20 @@
+import { ReqBoardWriteFilter } from "../../../lib/api/payloads/Board";
+
+const WRITE_NOTICE_SAGA = "write/WRITE_NOTICE_SAGA" as const;
+
+const writeNoticeSaga = (payload: ReqBoardWriteFilter) => ({
+  type: WRITE_NOTICE_SAGA,
+  payload
+});
+
+type WriteAction = ReturnType<typeof writeNoticeSaga>;
+
+export const writeAction = {
+  WRITE_NOTICE_SAGA
+};
+
+export const writeActionCreater = {
+  writeNoticeSaga
+};
+
+export default WriteAction;
---
+++ src/modules/saga/write/index.ts
@@ -0,0 +1,3 @@
+function* writeNoticeSaga() {}
+
+function* writeSaga() {}
