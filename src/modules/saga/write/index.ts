04a142dbab228c2f24cf488b2408b02754ddaa07
YeongG <yeonggil223@gmail.com>
2020-12-21 23:09:36 +0900

    [MODIFY] 공지 작성 saga

--- src/modules/saga/write/index.ts
+++ src/modules/saga/write/index.ts
@@ -1,3 +1,25 @@
-function* writeNoticeSaga() {}
+import { toast } from "react-toastify";
+import { call, takeEvery } from "redux-saga/effects";
+import { writeNotice } from "../../../lib/api/Write";
+import { writeAction, writeActionCreater } from "../../action/write";
 
-function* writeSaga() {}
+function* writeNoticeSaga(
+  action: ReturnType<typeof writeActionCreater.writeNoticeSaga>
+) {
+  try {
+    const { title, content, target_grade, target_group } = action.payload;
+    yield call(
+      writeNotice,
+      "school",
+      { title, content },
+      { target_grade, target_group }
+    );
+    toast.dark("공지 작성에 성공 했습니다");
+  } catch (err) {}
+}
+
+function* writeSaga() {
+  yield takeEvery(writeAction.WRITE_NOTICE_SAGA, writeNoticeSaga);
+}
+
+export default writeSaga;
