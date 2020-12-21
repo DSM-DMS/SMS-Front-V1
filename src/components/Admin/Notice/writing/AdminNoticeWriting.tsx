ad17d095460834a16eb638b8806017f4a4dd47cf
YeongG <yeonggil223@gmail.com>
2020-12-21 22:48:40 +0900

    [MODIFY] 공지 쓰기 필터 적용

--- src/components/Admin/Notice/writing/AdminNoticeWriting.tsx
+++ src/components/Admin/Notice/writing/AdminNoticeWriting.tsx
@@ -6,19 +6,29 @@ import React, {
   useCallback,
   ChangeEvent
 } from "react";
+import { toast, ToastContainer } from "react-toastify";
 import { NavIconNoticeMint } from "../../../../assets";
 import { PageHeader } from "../../../default";
 import * as S from "./styles";
 import EditerJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 import List from "@editorjs/list";
-import WriteCategory from "../../../default/Category/WriteCategory";
+import WriteCategory, {
+  BoardWriteFilterSate
+} from "../../../default/Category/WriteCategory";
 import { BoardWriteFilter } from "../../../../lib/api/payloads/Board";
+import { useDispatch } from "react-redux";
+import { writeActionCreater } from "../../../../modules/action/write";
+import { isIncludeEmpty } from "../../../../lib/utils";
 
 const AdminNoticeWriting: FC = () => {
+  const dispatch = useDispatch();
   const editerRef = useRef<EditerJS>();
-  const [headeText, setHeaderText] = useState<string>();
-  const [filterData, setFilterData] = useState<BoardWriteFilter>({});
+  const [title, setTitle] = useState<string>("");
+  const [filterData, setFilterData] = useState<BoardWriteFilter>({
+    target_grade: 0,
+    target_group: 0
+  });
 
   useEffect(() => {
     const editer = new EditerJS({
@@ -32,7 +42,7 @@ const AdminNoticeWriting: FC = () => {
   }, []);
 
   const changeHeaderText = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setHeaderText(e.target.value);
+    setTitle(e.target.value);
   }, []);
 
   const writeFilterHandler = useCallback((data: BoardWriteFilter) => {
@@ -41,16 +51,39 @@ const AdminNoticeWriting: FC = () => {
 
   const saveHandler = useCallback(async () => {
     if (!editerRef) return;
+    if (!title) {
+      toast.dark("제목을 입력하세요");
+      return;
+    }
     const writeData = await editerRef.current.save();
-  }, [filterData]);
+    if (!writeData.blocks.length) {
+      toast.dark("내용을 입력하세요");
+      return;
+    }
+
+    if (isIncludeEmpty(filterData)) {
+      toast.dark("필터를 적용해 주세요");
+      return;
+    }
+
+    const writeDataStr: string = JSON.stringify(writeData);
+    dispatch(
+      writeActionCreater.writeNoticeSaga({
+        content: writeDataStr,
+        title,
+        ...filterData
+      })
+    );
+  }, [title, filterData]);
 
   return (
     <S.Container>
+      <ToastContainer autoClose={2000} />
       <PageHeader imgSrc={NavIconNoticeMint} title="공지사항" type="DETAIL" />
       <S.Hr />
       <S.TitleInput
         onChange={changeHeaderText}
-        value={headeText}
+        value={title}
         type="text"
         placeholder="제목을 입력하세요"
       />
