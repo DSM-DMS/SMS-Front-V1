b35e46095c191841c62fdc9901f72eee426edcfb
YeongG <yeonggil223@gmail.com>
2020-12-22 23:34:12 +0900

    [MODIFY] PageNation추가

--- src/components/default/Board/Board.tsx
+++ src/components/default/Board/Board.tsx
@@ -3,6 +3,9 @@ import * as S from "./styles";
 import BoardTableHeader from "./Table/header/BoardTableHeader";
 import BoardTableBody from "./Table/body/BoardTableBody";
 import { BoardListItem } from "../../../lib/api/payloads/Board";
+import qs from "query-string";
+import PagiNation from "../PagiNation/PagiNation";
+import { useLocation } from "react-router-dom";
 
 export interface BoardObj {
   id: number;
@@ -18,10 +21,13 @@ interface Props {
 }
 
 const Board: FC<Props> = ({ data, names }) => {
+  const location = useLocation();
+  const page = Number(qs.parse(location.search).page) || 0;
   return (
     <S.Container>
       <BoardTableHeader names={names} />
       <BoardTableBody data={data} />
+      <PagiNation page={page} />
     </S.Container>
   );
 };
