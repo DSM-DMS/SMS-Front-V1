43ccb94843f47804903f9f330ac822a76f0fb5ba
YeongG <yeonggil223@gmail.com>
2020-12-22 23:35:54 +0900

    [MODIFY] outingCard action 정의

--- src/lib/api/payloads/OutingCard/index.ts
+++ src/lib/api/payloads/OutingCard/index.ts
@@ -1,4 +1,11 @@
-import { OutingStatus } from "../Outing";
+export type OutingActions =
+  | "start"
+  | "end"
+  | "teacher-approve"
+  | "teacher-reject"
+  | "certify"
+  | "parent-approve"
+  | "parent-reject";
 
 export interface ResOutingCardListItem {
   outing_uuid: string;
