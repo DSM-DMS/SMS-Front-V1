938929bb4faff2d4886793076fec2b4d3e747245
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-23 15:39:10 +0900

    [MODIFY] <LoginContainer> [user type]_uuid -> uuid

--- src/containers/Outing/HistoryContainer.tsx
+++ src/containers/Outing/HistoryContainer.tsx
@@ -39,7 +39,7 @@ const HistoryContainer: FC<Props> = (): ReactElement => {
     try {
       const {
         data: { outings }
-      } = await getHistory(localStorage.getItem("student_uuid"), historyStart);
+      } = await getHistory(localStorage.getItem("uuid"), historyStart);
 
       dispatch(setOutingHistoryList(outings));
       setHistoryStart(prev => (prev += 9));
