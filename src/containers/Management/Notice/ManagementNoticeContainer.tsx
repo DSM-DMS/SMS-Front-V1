481b35198e581261251f514cbc0833c57bf202ca
YeongG <yeonggil223@gmail.com>
2020-12-28 23:35:10 +0900

    [MODIFY] 관리자 공지사항 container

--- src/containers/Management/Notice/ManagementNoticeContainer.tsx
+++ src/containers/Management/Notice/ManagementNoticeContainer.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FC } from "react";
+import { useDispatch } from "react-redux";
+import { RouteChildrenProps } from "react-router-dom";
 import { ManagementNotice } from "../../../components";
+import { getCircleNoticeListSaga } from "../../../modules/action/notice";
+import qs from "query-string";
 
-const ManagementEditContainer: FC = () => {
+const ManagementEditContainer: FC<RouteChildrenProps> = ({ location }) => {
+  const dispatch = useDispatch();
+  const page = qs.parse(location.search).page || 0;
+
+  useEffect(() => {
+    dispatch(getCircleNoticeListSaga(Number(page)));
+  }, [page]);
   return <ManagementNotice />;
 };
 
