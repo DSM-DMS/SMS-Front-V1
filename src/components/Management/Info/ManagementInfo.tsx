bf416da09d39660825aff861ab40a3bd2a237687
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-30 15:18:18 +0900

    [MODIFY] <components/Management/Info/ManagementInfo> handler 생성

--- src/components/Management/Info/ManagementInfo.tsx
+++ src/components/Management/Info/ManagementInfo.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC } from "react";
+import { useSelector } from "react-redux";
 
 import * as S from "./style";
 import ClubConcept from "./Concept";
@@ -11,22 +12,62 @@ import ClubPicture from "./Picture";
 import ClubLocation from "./Location";
 import ManagementInfoBottom from "./Bottom";
 
-const ManagementInfo: FC = () => {
+import { stateType } from "../../../modules/reducer";
+import { ManagementInfoHandler } from "../../../modules/action/management/info";
+
+interface Props {}
+
+const ManagementInfo: FC<Props> = () => {
+  const handler = new ManagementInfoHandler();
+  const {
+    name,
+    field,
+    location,
+    club_concept,
+    introduction,
+    member_uuids,
+    leader_uuid,
+    logo_uri,
+    link
+  } = useSelector((state: stateType) => state.ManagementInfo);
+
+  const handleName = (e: ChangeEvent<HTMLInputElement>) => {
+    handler.handleName(e.target.value);
+  };
+
+  const handleConcept = (e: ChangeEvent<HTMLInputElement>) => {
+    handler.handleConcept(e.target.value);
+  };
+
+  const handleIntroduction = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    handler.handleIntroduce(e.target.value);
+  };
+
+  const handleLink = (e: ChangeEvent<HTMLInputElement>) => {
+    handler.handleFacebookLink(e.target.value);
+  };
+
   return (
     <S.ManagementInfoWrap>
       <S.TopLine />
       <S.Center>
         <S.CenterLeft>
-          <ClubName />
-          <ClubField />
-          <ClubLocation />
-          <ClubConcept />
-          <ClubIntroduce />
-          <ClubMembers />
+          <ClubName name={name} handleName={handleName} />
+          <ClubField field={field} />
+          <ClubLocation location={location} />
+          <ClubConcept
+            clubConcept={club_concept}
+            handleConcept={handleConcept}
+          />
+          <ClubIntroduce
+            introduction={introduction}
+            handleIntroduction={handleIntroduction}
+          />
+          <ClubMembers leaderUuid={leader_uuid} memberUuids={member_uuids} />
         </S.CenterLeft>
         <S.CenterRight>
-          <ClubPicture />
-          <ClubFacebookLink />
+          <ClubPicture logoUri={logo_uri} />
+          <ClubFacebookLink link={link} handleLink={handleLink} />
         </S.CenterRight>
       </S.Center>
       <ManagementInfoBottom />
