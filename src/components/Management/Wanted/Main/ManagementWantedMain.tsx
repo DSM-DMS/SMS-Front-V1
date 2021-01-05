c0062ad1bebc248564f90ae0e514bfd9efcc1148
YeongG <yeonggil223@gmail.com>
2021-01-05 09:09:11 +0900

    [MODIFY] 페북 버튼 클릭시 새탭

--- src/components/Management/Wanted/Main/ManagementWantedMain.tsx
+++ src/components/Management/Wanted/Main/ManagementWantedMain.tsx
@@ -1,18 +1,24 @@
 import React, { FC } from "react";
 import { useSelector } from "react-redux";
 import { FacebookIcon } from "../../../../assets";
+import { getFacebookLink } from "../../../../lib/utils";
 import { stateType } from "../../../../modules/reducer";
 import * as S from "../../../../styles/CircleWantedDetail";
 import { FaceBookBtn } from "../../../default/Info/Header/styles";
 import ManagementWantedContent from "./Content/ManagementWantedContent";
 
 const ManagementWantedMain: FC = () => {
-  const { name, link } = useSelector((store: stateType) => store.management);
+  const { name, link, club_concept } = useSelector(
+    (store: stateType) => store.management
+  );
   return (
     <S.Main>
       <S.Header>
-        <S.CircleName>{name}</S.CircleName>
-        <FaceBookBtn href={link}>
+        <div>
+          <S.CircleName>{name}</S.CircleName>
+          <p>{club_concept}</p>
+        </div>
+        <FaceBookBtn target="_blank" href={getFacebookLink(link)}>
           <img src={FacebookIcon} />
           지원하기
         </FaceBookBtn>
