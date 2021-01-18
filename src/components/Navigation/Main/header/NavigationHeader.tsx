530ac7231d4d7416cdc487b60b6d39bd1005ae94
YeongG <yeonggil223@gmail.com>
2021-01-18 19:27:53 +0900

    [MODIFY] 동아리 로고 클릭시 이동

--- src/components/Navigation/Main/header/NavigationHeader.tsx
+++ src/components/Navigation/Main/header/NavigationHeader.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { FC } from "react";
+import React, { FC } from "react";
 import * as S from "./styles";
 import { IconBlack, IconWhite } from "../../../../assets/";
+import { Link } from "react-router-dom";
 
 interface Props {
   isManagementMode: boolean;
@@ -11,8 +11,10 @@ const NavigationHeader: FC<Props> = ({ isManagementMode }) => {
   return (
     <S.Container>
       <S.InnerContainer>
-        <S.LogoImg src={isManagementMode ? IconBlack : IconWhite} />
-        <S.LogoText>SMS</S.LogoText>
+        <Link to="/home">
+          <S.LogoImg src={isManagementMode ? IconBlack : IconWhite} />
+          <S.LogoText>SMS</S.LogoText>
+        </Link>
       </S.InnerContainer>
       {isManagementMode && <div>Club Admin Service</div>}
     </S.Container>
