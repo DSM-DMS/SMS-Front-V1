0a34f72ce64d636e7ab2b8dc7d6be88dc5ef1ed5
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-01-20 18:12:08 +0900

    [MODIFY] 시간표 없는 부분 공백 -> ' - ' 로 대체

--- src/components/Main/Timetable/TimeTableList.tsx
+++ src/components/Main/Timetable/TimeTableList.tsx
@@ -20,31 +20,31 @@ const TimetableList: FC<Props> = ({ timetable, loading }) => {
         <>
           <S.TimetableItem>
             <S.TimetableItemDate>1교시</S.TimetableItemDate>
-            {time1}
+            {time1 ? time1 : "-"}
           </S.TimetableItem>
           <S.TimetableItem>
             <S.TimetableItemDate>2교시</S.TimetableItemDate>
-            {time2}
+            {time2 ? time2 : "-"}
           </S.TimetableItem>
           <S.TimetableItem>
             <S.TimetableItemDate>3교시</S.TimetableItemDate>
-            {time3}
+            {time3 ? time3 : "-"}
           </S.TimetableItem>
           <S.TimetableItem>
             <S.TimetableItemDate>4교시</S.TimetableItemDate>
-            {time4}
+            {time4 ? time4 : "-"}
           </S.TimetableItem>
           <S.TimetableItem>
             <S.TimetableItemDate>5교시</S.TimetableItemDate>
-            {time5}
+            {time5 ? time5 : "-"}
           </S.TimetableItem>
           <S.TimetableItem>
             <S.TimetableItemDate>6교시</S.TimetableItemDate>
-            {time6}
+            {time6 ? time6 : "-"}
           </S.TimetableItem>
           <S.TimetableItem>
             <S.TimetableItemDate>7교시</S.TimetableItemDate>
-            {time7}
+            {time7 ? time7 : "-"}
           </S.TimetableItem>
         </>
       )}
