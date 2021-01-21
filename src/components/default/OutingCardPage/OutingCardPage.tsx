399bec35d9838d290205c0d6b4a621ad59a796c6
YeongG <yeonggil223@gmail.com>
2021-01-21 22:15:21 +0900

    [MODIFY] prettier로 인한 코드 변경

--- src/components/default/OutingCardPage/OutingCardPage.tsx
+++ src/components/default/OutingCardPage/OutingCardPage.tsx
@@ -1,8 +1,6 @@
 import React, { FC, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
 import * as S from "./styles";
-
 import OutingCard from "../OutingCard/OutingCard";
 import OutingCardFilter from "../Filter/OutingCardFilter";
 import OutingCardModal from "../Modal/OutingCardModal/OutingCardModal";
@@ -16,7 +14,7 @@ import { getOutingCardListSaga } from "../../../modules/action/outingCard";
 interface Props {
   title: string;
   status: number;
-  isClicked: (...arg: any) => void;
+  isClicked?: (...arg: any) => void;
 }
 
 const OutingCardPage: FC<Props> = ({ title, isClicked, status }) => {
