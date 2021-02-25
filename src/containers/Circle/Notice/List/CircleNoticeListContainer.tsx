230a2d53b0bf21358edcbb37a66007c320e722e9
YeongG <yeonggil223@gmail.com>
2021-02-25 19:09:52 +0900

    [MODIFY] 공지사항 검색 기능 추가

--- src/containers/Circle/Notice/List/CircleNoticeListContainer.tsx
+++ src/containers/Circle/Notice/List/CircleNoticeListContainer.tsx
@@ -1,15 +1,25 @@
 import React, { FC, useEffect } from "react";
+import qs from "query-string";
 import { CircleNoticeList } from "../../../../components";
 import { useDispatch } from "react-redux";
 import { RouteChildrenProps } from "react-router-dom";
-import { getNoticeClubList } from "../../../../modules/action/notice/list";
+import {
+  getNoticeClubList,
+  searchNoticeClubList
+} from "../../../../modules/action/notice/list";
 
 const CircleNoticeListContainer: FC<RouteChildrenProps> = ({ match }) => {
-  const disaptch = useDispatch();
+  const dispatch = useDispatch();
   const page: number = Number((match.params as any).page) || 0;
+  const query: string = (qs.parse(location.search).search as string) || "";
+
   useEffect(() => {
-    disaptch(getNoticeClubList(page));
-  }, [page]);
+    if (query.length > 0) {
+      dispatch(searchNoticeClubList({ query, page }));
+      return;
+    }
+    dispatch(getNoticeClubList(page));
+  }, [page, query]);
   return <CircleNoticeList />;
 };
 
--- src/containers/Management/Notice/ManagementNoticeContainer.tsx
+++ src/containers/Management/Notice/ManagementNoticeContainer.tsx
@@ -3,15 +3,23 @@ import { useDispatch } from "react-redux";
 import { RouteChildrenProps } from "react-router-dom";
 import { ManagementNotice } from "../../../components";
 import qs from "query-string";
-import { getNoticeClubList } from "../../../modules/action/notice/list";
+import {
+  getNoticeClubList,
+  searchNoticeClubList
+} from "../../../modules/action/notice/list";
 
 const ManagementEditContainer: FC<RouteChildrenProps> = ({ location }) => {
   const dispatch = useDispatch();
   const page: number = Number(qs.parse(location.search).page) || 0;
+  const query: string = (qs.parse(location.search).search as string) || "";
 
   useEffect(() => {
+    if (query.length > 0) {
+      dispatch(searchNoticeClubList({ query, page }));
+      return;
+    }
     dispatch(getNoticeClubList(page));
-  }, [page]);
+  }, [page, query]);
   return <ManagementNotice />;
 };
 
