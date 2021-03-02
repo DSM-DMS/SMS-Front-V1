fb17f59cdecac09b52763766875a1d758fd87e21
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-03-02 12:04:17 +0900

    [MODIFY] <lib/api/Login> 사용하지 않는 함수 제거

--- src/lib/api/Login/index.ts
+++ src/lib/api/Login/index.ts
@@ -1,18 +1,9 @@
 import { apiDefault } from "../client";
-import {
-  ResStudentLoginWithDefault,
-  ResStudentInfoWithDefault
-} from "../payloads/Login";
+import { ResStudentLoginWithDefault } from "../payloads/Login";
 
 export const postLoginStudent = (id: string, pw: string) => {
   return apiDefault().post<ResStudentLoginWithDefault>("/login/student", {
     student_id: id,
     student_pw: pw
   });
-};
-
-export const getStudentInfo = (studentUuid: string) => {
-  return apiDefault().get<ResStudentInfoWithDefault>(
-    `/students/uuid/${studentUuid}`
-  );
 };
