92087f2f517401d1f3b060f1e4239af18c7f8afc
YeongG <yeonggil223@gmail.com>
2021-03-02 13:41:56 +0900

    [MODIFY] 로그인 안됬을시 내비게이션 안되게 처리

--- src/components/Navigation/Item/MainNavigationItemContainer.tsx
+++ src/components/Navigation/Item/MainNavigationItemContainer.tsx
@@ -1,8 +1,10 @@
 import React, { FC, useCallback, memo } from "react";
 import NavigationItem from "./NavigationItem";
 import { useHistory } from "react-router";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { pageMove } from "../../../modules/action/page";
+import { stateType } from "../../../modules/reducer";
+import { toast } from "react-toastify";
 
 interface Props {
   name: string;
@@ -17,15 +19,20 @@ const MainNavigationItemContainer: FC<Props> = ({
   src,
   route
 }) => {
+  const data = useSelector((store: stateType) => store.header);
   const history = useHistory();
   const dispatch = useDispatch();
 
   const onClick = useCallback(() => {
+    if (data.name.length === 0) {
+      toast.error("로그인을 진행해 주세요");
+      return;
+    }
     if (history.location.pathname === route) return;
 
     dispatch(pageMove(name));
     history.push(route);
-  }, [dispatch, isActive]);
+  }, [dispatch, route, data.name]);
 
   return (
     <NavigationItem
