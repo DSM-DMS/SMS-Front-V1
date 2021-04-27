d7fc5d20933ca8812868045555444daddfed3900
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-27 16:54:47 +0900

    [MODIFY] <lib/hooks/useApplyTime> 시간 제한 없애기

--- src/lib/hooks/useApplyTime.ts
+++ src/lib/hooks/useApplyTime.ts
@@ -1,37 +1,19 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { OnChangeEvent } from "./common/useInput";
 
 const useApplyTime = () => {
-  const [outTime, setOut] = useState<string>(":");
-  const [inTime, setIn] = useState<string>(":");
+  const [outTime, setOut] = useState<string>("");
+  const [inTime, setIn] = useState<string>("");
 
-  const handleOutHour = (value: string) => {
-    const split = outTime.split(":");
-    setOut(`${value}:${split[1]}`);
-  };
+  const onChangeOut = useCallback((e: OnChangeEvent) => {
+    setOut(e.currentTarget.value);
+  }, []);
 
-  const handleOutMin = (value: string) => {
-    const split = outTime.split(":");
-    setOut(`${split[0]}:${value}`);
-  };
+  const onChangeIn = useCallback((e: OnChangeEvent) => {
+    setIn(e.currentTarget.value);
+  }, []);
 
-  const handleInHour = (value: string) => {
-    const split = inTime.split(":");
-    setIn(`${value}:${split[1]}`);
-  };
-
-  const handleInMin = (value: string) => {
-    const split = inTime.split(":");
-    setIn(`${split[0]}:${value}`);
-  };
-
-  return [
-    outTime,
-    inTime,
-    handleOutHour,
-    handleOutMin,
-    handleInHour,
-    handleInMin
-  ] as const;
+  return [outTime, inTime, onChangeOut, onChangeIn] as const;
 };
 
 export default useApplyTime;
